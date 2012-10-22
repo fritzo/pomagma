3e6cf70d38cac9b140bfef9ce26a03fded946c2c
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2012-10-22 13:06:54 -0700

    Implement bounded depth ob sampler (not working)

--- src/grower/sampler.cpp
+++ src/grower/sampler.cpp
@@ -7,6 +7,9 @@
 namespace pomagma
 {
 
+//----------------------------------------------------------------------------
+// construction
+
 Sampler::Sampler (Carrier & carrier)
     : m_carrier(carrier)
 {
@@ -52,6 +55,24 @@ inline float sum (const std::unordered_map<T, float> & map)
         result += pair.second;
     }
     return result;
+}
+
+template<class Key>
+inline Key sample (
+        const std::unordered_map<Key, float> & probs,
+        float total)
+{
+    while (true) {
+        float r = random_01() * total;
+        for (const auto & pair : probs) {
+            Key key = pair.first;
+            float prob = pair.second;
+            if ((r -= prob) < 0) {
+                return key;
+            }
+        }
+        // occasionally fall through due to rounding error
+    }
 }
 
 void Sampler::declare (const std::string & name, const NullaryFunction & fun)
@@ -125,121 +146,185 @@ void Sampler::set_prob (const std::string & name, float prob)
         try_set_prob_(m_binary_funs, name, prob) or
         try_set_prob_(m_symmetric_funs, name, prob);
     POMAGMA_ASSERT(found, "failed to set prob of function: " << name);
+    m_bounded_samplers.clear();
 }
 
+//----------------------------------------------------------------------------
+// sampling
 
-template<class Key>
-inline Key sample (
-        const std::unordered_map<Key, float> & probs,
-        float total)
+inline Sampler::Arity Sampler::BoundedSampler::sample_arity () const
 {
     while (true) {
         float r = random_01() * total;
-        for (const auto & pair : probs) {
-            Key key = pair.first;
-            float prob = pair.second;
-            if ((r -= prob) < 0) {
-                return key;
-            }
-        }
-
+        if ((r -= nullary) < 0) return NULLARY;
+        if ((r -= injective) < 0) return INJECTIVE;
+        if ((r -= binary) < 0) return BINARY;
+        if ((r -= symmetric) < 0) return SYMMETRIC;
         // occasionally fall through due to rounding error
     }
-};
-
-bool Sampler::try_insert_random () const
-{
-    size_t attempt = 0;
-    while (m_carrier.item_count() < m_carrier.item_dim()) {
-        auto pair = try_insert_random_();
-        bool inserted = pair.second;
-        if (inserted) {
-            return true;
-        }
-        if (++attempt % 10000 == 0) {
-            POMAGMA_WARN("failed " << attempt << " insertion attempts");
-        }
-    }
-    return false;
 }
 
-std::pair<Ob, bool> Sampler::try_insert_random_ () const
+inline Sampler::Arity Sampler::BoundedSampler::sample_compound_arity () const
 {
-    size_t attempt = 0;
     while (true) {
-        float r = random_01();
-
-        if ((r -= m_nullary_prob) < 0) {
-            auto & fun = * sample(m_nullary_probs, m_nullary_prob);
-            if (Ob val = fun.find()) {
-                val = m_carrier.find(val);
-                return std::make_pair(val, false);
-            } else {
-                if (Ob val = m_carrier.try_insert()) {
-                    fun.insert(val);
-                    return std::make_pair(val, true);
-                } else {
-                    return std::make_pair(0, false);
-                }
-            }
-        }
-
-        auto arg1_inserted = try_insert_random_();
-        if (arg1_inserted.second) return arg1_inserted;
-        Ob arg1 = arg1_inserted.first;
-
-        if ((r -= m_injective_prob) < 0) {
-            auto & fun = * sample(m_injective_probs, m_injective_prob);
-            if (Ob val = fun.find(arg1)) {
-                val = m_carrier.find(val);
-                return std::make_pair(val, false);
-            } else {
-                if (Ob val = m_carrier.try_insert()) {
-                    fun.insert(arg1, val);
-                    return std::make_pair(val, true);
-                } else {
-                    return std::make_pair(0, false);
-                }
-            }
-        }
-
-        auto arg2_inserted = try_insert_random_();
-        if (arg2_inserted.second) return arg2_inserted;
-        Ob arg2 = arg2_inserted.first;
-
-        if ((r -= m_binary_prob) < 0) {
-            auto & fun = * sample(m_binary_probs, m_binary_prob);
-            if (Ob val = fun.find(arg1, arg2)) {
-                val = m_carrier.find(val);
-                return std::make_pair(val, false);
-            } else {
-                if (Ob val = m_carrier.try_insert()) {
-                    fun.insert(arg1, arg2, val);
-                    return std::make_pair(val, true);
-                } else {
-                    return std::make_pair(0, false);
-                }
-            }
-        }
-
-        if ((r -= m_symmetric_prob) < 0) {
-            auto & fun = * sample(m_symmetric_probs, m_symmetric_prob);
-            if (Ob val = fun.find(arg1, arg2)) {
-                val = m_carrier.find(val);
-                return std::make_pair(val, false);
-            } else {
-                if (Ob val = m_carrier.try_insert()) {
-                    fun.insert(arg1, arg2, val);
-                    return std::make_pair(val, true);
-                } else {
-                    return std::make_pair(0, false);
-                }
-            }
-        }
-
+        float r = random_01() * (total - nullary);
+        if ((r -= injective) < 0) return INJECTIVE;
+        if ((r -= binary) < 0) return BINARY;
+        if ((r -= symmetric) < 0) return SYMMETRIC;
         // occasionally fall through due to rounding error
-        if (++attempt % 10 == 0) {
-            POMAGMA_WARN("failed " << attempt << " insertion attempts");
+    }
+}
+
+inline const Sampler::BoundedSampler & Sampler::bounded_sampler (
+        size_t max_depth) const
+{
+    if (unlikely(max_depth >= m_bounded_samplers.size())) {
+        if (unlikely(m_bounded_samplers.empty())) {
+            m_bounded_samplers.push_back(BoundedSampler(*this));
+        } else {
+            m_bounded_samplers.push_back(
+                    BoundedSampler(*this, m_bounded_samplers.back()));
+        }
+    }
+
+    return m_bounded_samplers[max_depth];
+}
+
+Ob Sampler::try_insert_random () const
+{
+    try {
+        Ob ob = try_insert_random_nullary();
+        for (size_t depth = 1; depth; ++depth) {
+            ob = try_insert_random_compound(ob, depth - 1);
+        }
+    } catch (InsertException e) {
+        return e.inserted;
+    }
+
+    POMAGMA_ERROR("sampler failed");
+    return 0;
+}
+
+inline Ob Sampler::try_insert_random_compound (Ob ob, size_t max_depth) const
+{
+    const BoundedSampler & sampler = bounded_sampler(max_depth);
+    Arity arity = sampler.sample_compound_arity();
+    switch (arity) {
+        case NULLARY: {
+            POMAGMA_ERROR("unreachable");
+        } break;
+
+        case INJECTIVE: {
+            return try_insert_random_injective(ob);
+        } break;
+
+        case BINARY: {
+            Ob other = try_insert_random(max_depth);
+            if (random_bool(0.5)) std::swap(ob, other);
+            return try_insert_random_binary(ob, other);
+        } break;
+
+        case SYMMETRIC: {
+            Ob other = try_insert_random(max_depth);
+            return try_insert_random_symmetric(ob, other);
+        } break;
+    }
+
+    POMAGMA_ERROR("unreachable");
+    return 0;
+}
+
+Ob Sampler::try_insert_random (size_t max_depth) const
+{
+    const BoundedSampler & sampler = bounded_sampler(max_depth);
+    Arity arity = sampler.sample_arity();
+    switch (arity) {
+        case NULLARY: {
+            return try_insert_random_nullary();
+        } break;
+
+        case INJECTIVE: {
+            Ob key = try_insert_random(max_depth);
+            return try_insert_random_injective(key);
+        } break;
+
+        case BINARY: {
+            Ob lhs = try_insert_random(max_depth);
+            Ob rhs = try_insert_random(max_depth);
+            return try_insert_random_binary(lhs, rhs);
+        } break;
+
+        case SYMMETRIC: {
+            Ob lhs = try_insert_random(max_depth);
+            Ob rhs = try_insert_random(max_depth);
+            return try_insert_random_symmetric(lhs, rhs);
+        } break;
+    }
+
+    POMAGMA_ERROR("unreachable");
+    return 0;
+}
+
+Ob Sampler::try_insert_random_nullary () const
+{
+    auto & fun = * sample(m_nullary_probs, m_nullary_prob);
+    if (Ob val = fun.find()) {
+        val = m_carrier.find(val);
+        return val;
+    } else {
+        if (Ob val = m_carrier.try_insert()) {
+            fun.insert(val);
+            throw(InsertException(val));
+        } else {
+            throw(InsertException(0));
+        }
+    }
+}
+
+Ob Sampler::try_insert_random_injective (Ob key) const
+{
+    auto & fun = * sample(m_injective_probs, m_injective_prob);
+    if (Ob val = fun.find(key)) {
+        val = m_carrier.find(val);
+        return val;
+    } else {
+        if (Ob val = m_carrier.try_insert()) {
+            fun.insert(key, val);
+            throw(InsertException(val));
+        } else {
+            throw(InsertException(0));
+        }
+    }
+}
+
+Ob Sampler::try_insert_random_binary (Ob lhs, Ob rhs) const
+{
+    auto & fun = * sample(m_binary_probs, m_binary_prob);
+    if (Ob val = fun.find(lhs, rhs)) {
+        val = m_carrier.find(val);
+        return val;
+    } else {
+        if (Ob val = m_carrier.try_insert()) {
+            fun.insert(lhs, rhs, val);
+            throw(InsertException(val));
+        } else {
+            throw(InsertException(0));
+        }
+    }
+}
+
+Ob Sampler::try_insert_random_symmetric (Ob lhs, Ob rhs) const
+{
+    auto & fun = * sample(m_symmetric_probs, m_symmetric_prob);
+    if (Ob val = fun.find(lhs, rhs)) {
+        val = m_carrier.find(val);
+        return val;
+    } else {
+        if (Ob val = m_carrier.try_insert()) {
+            fun.insert(lhs, rhs, val);
+            throw(InsertException(val));
+        } else {
+            throw(InsertException(0));
         }
     }
 }
--- src/grower/sampler.hpp
+++ src/grower/sampler.hpp
@@ -24,10 +24,47 @@ class Sampler
     std::unordered_map<const InjectiveFunction *, float> m_injective_probs;
     std::unordered_map<const BinaryFunction *, float> m_binary_probs;
     std::unordered_map<const SymmetricFunction *, float> m_symmetric_probs;
+
     float m_nullary_prob;
     float m_injective_prob;
     float m_binary_prob;
     float m_symmetric_prob;
+
+    enum Arity { NULLARY, INJECTIVE, BINARY, SYMMETRIC };
+    struct BoundedSampler
+    {
+        float nullary;
+        float injective;
+        float binary;
+        float symmetric;
+        float total;
+
+        BoundedSampler () {}
+
+        // base case
+        BoundedSampler (const Sampler & sampler)
+            : nullary(sampler.m_nullary_prob),
+              injective(0),
+              binary(0),
+              symmetric(0),
+              total(nullary)
+        {
+        }
+        // induction step
+        BoundedSampler (const Sampler & sampler, const BoundedSampler & prev)
+            : nullary(sampler.m_nullary_prob),
+              injective(sampler.m_injective_prob * prev.total),
+              binary(sampler.m_binary_prob * (prev.total * prev.total)),
+              symmetric(sampler.m_symmetric_prob * (prev.total * prev.total)),
+              total(nullary + injective + binary + symmetric)
+        {
+        }
+
+        Arity sample_arity () const;
+        Arity sample_compound_arity () const;
+    };
+    mutable std::vector<BoundedSampler> m_bounded_samplers;
+    const BoundedSampler & bounded_sampler (size_t max_depth) const;
 
 public:
 
@@ -42,7 +79,7 @@ public:
 
     void set_prob (const std::string &, float prob);
 
-    bool try_insert_random () const;
+    Ob try_insert_random () const;
 
 private:
 
@@ -57,7 +94,17 @@ private:
             const std::string & name,
             float prob);
 
-    std::pair<Ob, bool> try_insert_random_ () const;
+    struct InsertException
+    {
+        Ob inserted;
+        InsertException (Ob i) : inserted(i) {}
+    };
+    Ob try_insert_random_compound (Ob ob, size_t max_depth) const;
+    Ob try_insert_random (size_t max_depth) const;
+    Ob try_insert_random_nullary () const;
+    Ob try_insert_random_injective (Ob key) const;
+    Ob try_insert_random_binary (Ob lhs, Ob rhs) const;
+    Ob try_insert_random_symmetric (Ob lhs, Ob rhs) const;
 };
 
 } // namespace pomagma
