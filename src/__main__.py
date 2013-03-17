e24910d05495b3e619958b4b1d6cd5a9bb997ae2
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-03-17 12:00:38 -0700

    Convert makefile tests to python

--- src/__main__.py
+++ src/__main__.py
@@ -1,53 +1,12 @@
 import os
 import sys
+import shutil
 import subprocess
 import contextlib
-import tables
 import parsable
-
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-debug = 'POMAGMA_DEBUG' in os.environ
-if debug:
-    print 'Running in debug mode'
-    BUILD = os.path.join(ROOT, 'build', 'debug')
-    LOG_SUFFIX = '.debug.log'
-else:
-    BUILD = os.path.join(ROOT, 'build', 'release')
-    LOG_SUFFIX = '.log'
-BIN = os.path.join(BUILD, 'src')
-LOG = os.path.join(ROOT, 'log')
-
-THEORIES = ['h4', 'sk', 'skj']
-
-
-def make_env(**kwargs):
-    kwargs.setdefault('log_level', 4 if debug else 0)
-    env = os.environ.copy()
-    for key, val in kwargs.iteritems():
-        pomagma_key = 'POMAGMA_{}'.format(key.upper())
-        sys.stderr.write('{} = {} \\\n'.format(pomagma_key, val))
-        env[pomagma_key] = str(val)
-    return env
-
-
-def call(*args, **kwargs):
-    env = make_env(**kwargs)
-    log_file = env['POMAGMA_LOG_FILE']
-    sys.stderr.write('{}\n'.format(' \\\n'.join(args)))
-    info = subprocess.call(args, env=env)
-    if info:
-        subprocess.call(['grep', '-C3', '-i', 'error', log_file])
-    return info
-
-
-@contextlib.contextmanager
-def cd(path):
-    old_path = os.path.abspath(os.path.curdir)
-    try:
-        os.chdir(path)
-        yield
-    finally:
-        os.chdir(old_path)
+import pomagma.util
+import pomagma.wrapper
+import pomagma.batch
 
 
 @parsable.command
@@ -55,18 +14,7 @@ def build():
     '''
     Build pomagma tools from source.
     '''
-    with cd(BUILD):
-        call('cmake', ROOT)
-        call('make')
-
-
-def count_obs(structure):
-    if isinstance(structure, basestring):
-        structure = tables.openFile(structure)
-    support = structure.getNode('/carrier/support')
-    word_dim, = support.shape
-    item_dim = 64 * word_dim - 1
-    return item_dim  # only an upper bound on ob count
+    pomagma.util.build()
 
 
 @parsable.command
@@ -74,44 +22,53 @@ def info(infile):
     '''
     Print information about a structure file.
     '''
-    structure = tables.openFile(infile)
-    item_count = count_obs(structure)
-    print 'ob count <=', item_count
-    for o in structure:
-        print o
+    pomagma.util.print_info(infile)
 
 
 @parsable.command
-def init(theory, infile, size_kobs=1):
+def unit_test():
     '''
-    Initialize a structure with size kobs.
+    Run unit tests.
     '''
-    assert theory in THEORIES, 'unknown theory: {}'.format(theory)
-    tool = os.path.join(BIN, 'grower', '{}.grow'.format(theory))
-    log_file = os.path.join(LOG, theory + LOG_SUFFIX)
-    size = size_kobs * 1024 - 1
-    call(tool, infile, size=size, log_file=log_file)
+    buildtype = 'debug' if pomagma.util.debug else 'release'
+    opts = {
+        'log_file': os.path.join(pomagma.util.LOG, buildtype + '.test.log'),
+        'log_level': 3,
+        }
+    pomagma.util.build('test', **opts)
 
 
 @parsable.command
-def grow(theory, infile, outfile, size_kobs=1):
+def test_batch_test(theory='all'):
     '''
-    Grow a structure by kobs.
+    Test batch operations.
     '''
-    tool = os.path.join(BIN, 'grower', '{}.grow'.format(theory))
-    log_file = os.path.join(LOG, theory + LOG_SUFFIX)
-    size = count_obs(infile) + 1024 * size_kobs
-    call(tool, infile, outfile, size=size, log_file=log_file)
+    if theory == 'all':
+        theories = pomagma.util.MIN_SIZES.keys()
+        theories.sort(key=pomagma.util.MIN_SIZES.__getitem__)
+        for theory in theories:
+            test_batch(theory)
+    else:
+        theories = [theory]
+
+    print '-' * 78
+    print 'Building'
+    pomagma.util.build()
+    for theory in theories:
+        print '-' * 78
+        print 'Testing', theory
+        pomagma.batch.test(theory)
 
 
 @parsable.command
-def aggregate(theory, atlas_in, chart_in, atlas_out):
+def clean():
     '''
-    Grow a structure by kobs.
+    Clean out temporary files: build, log, data
     '''
-    tool = os.path.join(BIN, 'atlas', 'free.aggregate')
-    log_file = os.path.join(LOG, theory + LOG_SUFFIX)
-    call(tool, atlas_in, chart_in, atlas_out, log_file=log_file)
+    with pomagma.util.chdir(pomagma.util.ROOT):
+        for temp in ['build', 'log', 'data']:
+            if os.path.exists(temp):
+                shutil.rmtree(temp)
 
 
 if __name__ == '__main__':
--- src/util.py
+++ src/util.py
@@ -1,28 +1,96 @@
+import os
+import sys
+import subprocess
+import multiprocessing
+import contextlib
 import tables
-import parsable
+
+SRC = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.dirname(SRC)
+DATA = os.path.join(ROOT, 'data')
+LOG = os.path.join(ROOT, 'log')
+debug = 'POMAGMA_DEBUG' in os.environ
+if debug:
+    print 'Running in debug mode'
+    BUILD = os.path.join(ROOT, 'build', 'debug')
+else:
+    BUILD = os.path.join(ROOT, 'build', 'release')
+BIN = os.path.join(BUILD, 'src')
+
+GROWERS = {
+    'h4': 'h4.grow',
+    'sk': 'sk.grow',
+    'skj': 'skj.grow',
+    }
+
+MIN_SIZES = {
+    'h4': 127,
+    'sk': 1023,
+    'skj': 1535,
+    }
 
 
-parsable_commands = []
-def parsable_command(fun):
-    parsable_commands.append(fun)
-    return fun
+@contextlib.contextmanager
+def chdir(path):
+    old_path = os.path.abspath(os.path.curdir)
+    try:
+        #os.makedirs(path)
+        sys.stderr.write('cd {}\n'.format(path))
+        os.chdir(path)
+        yield
+    finally:
+        sys.stderr.write('cd {}\n'.format(old_path))
+        os.chdir(old_path)
 
 
-def parsable_dispatch():
-    for fun in parsable_commands:
-        parsable.command(fun)
-    parsable.dispatch()
+def abspath(path):
+    return os.path.abspath(os.path.expanduser(path))
 
 
-@parsable_command
-def hdf5_ls(filename):
-    '''
-    Prints directory of hdf5 file
-    '''
-    f = tables.openFile(filename)
-    for o in f:
+def make_env(**kwargs):
+    kwargs['root'] = ROOT
+    kwargs.setdefault('threads', multiprocessing.cpu_count())
+    log_file = os.path.join(LOG, 'default.log')
+    log_file = abspath(kwargs.get('log_file', log_file))
+    kwargs['log_file'] = log_file
+    kwargs.setdefault('log_level', 4 if debug else 0)
+    env = os.environ.copy()
+    for key, val in kwargs.iteritems():
+        pomagma_key = 'POMAGMA_{}'.format(key.upper())
+        sys.stderr.write('{}={} \\\n'.format(pomagma_key, val))
+        env[pomagma_key] = str(val)
+    return env
+
+
+def check_call(*args, **kwargs):
+    env = make_env(**kwargs)
+    sys.stderr.write('{}\n'.format(' \\\n'.join(args)))
+    subprocess.check_call(args, env=env)
+
+
+def build(*args, **kwargs):
+    buildtype = 'Debug' if debug else 'RelWithDebInfo'
+    buildflag = '-DCMAKE_BUILD_TYPE={}'.format(buildtype)
+    if not os.path.exists(BUILD):
+        os.makedirs(BUILD)
+    with chdir(BUILD):
+        check_call('cmake', buildflag, ROOT)
+        check_call('make', *args)
+
+
+def count_obs(structure):
+    # TODO return item_count rather than item_dim
+    if isinstance(structure, basestring):
+        structure = tables.openFile(structure)
+    support = structure.getNode('/carrier/support')
+    word_dim, = support.shape
+    item_dim = 64 * word_dim - 1
+    return item_dim  # only an upper bound on ob count
+
+
+def print_info(infile):
+    structure = tables.openFile(infile)
+    item_count = count_obs(structure)
+    print 'ob count <=', item_count
+    for o in structure:
         print o
-
-
-if __name__ == '__main__':
-    parsable_dispatch()
