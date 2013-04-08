304984442ce7ecd59aeeca35b28445b03d26cbcd
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-04-07 22:40:38 -0700

    Sketch boto.swf workflow for grower

---
+++ src/workflow/grow.py
@@ -0,0 +1,143 @@
+import simplejson as json
+import parsable
+import pomagma.workflow.util
+
+
+def TODO(message):
+    raise NotImplementedError('TODO {}'.format(message))
+
+
+def iter_recent_events(decision_task):
+    '''
+    Reverse-chronologically iterats over events since the last decision.
+    '''
+    prev_id = decision_task['previousStartedEventId']
+    events = decision_task['events']
+    for event in reversed(events):
+        if event['eventType'] == 'DecisionTaskStarted':
+            if event['eventId'] == prev_id:
+                break
+        yield event
+
+
+def find_schedule_event(events, activity_id):
+    for event in reversed(events):
+        if event['eventType'] == 'ActivityTaskScheduled':
+            if event['activityId'] == activity_id:
+                return event
+
+
+@parsable.command
+def start_decider():
+    '''
+    Start grow decider, typically on master node.
+    '''
+    name = 'Grow'
+    pomagma.workflow.util.register_domain()
+    pomagma.workflow.util.register_workflow_type(name)
+    while True:
+        task = pomagma.workflow.util.poll_decision_task(name)
+        events = [
+            e for e in iter_recent_events(task)
+            if not e['eventType'].startswith('Decision')
+            ]
+
+        last_event = events[0]
+        event_type = last_event['eventType']
+
+        if event_type == 'WorkflowExecutionStarted':
+            activity_type = 'Trim'
+            input = last_event['input']
+            pomagma.workflow.util.decide_to_schedule(activity_type, input)
+
+        elif event_type == 'ActivityTaskCompleted':
+            result = json.loads(last_event.get('result', ''))
+            nextActivity = result.get('nextActivity')
+            if nextActivity:
+                activity_type = nextActivity['activityType']
+                json_input = nextActivity['input']
+                input = json.dumps(json_input)
+                pomagma.workflow.util.decide_to_schedule(activity_type, input)
+            else:
+                pomagma.workflow.util.decide_to_complete(task)
+
+
+@parsable.command
+def start_trimmer():
+    '''
+    Start trimmer, typically on a high-memory node.
+    '''
+    name = 'Trim'
+    pomagma.workflow.util.register_activity_type(name)
+    while True:
+        task = pomagma.workflow.util.poll_activity_task(name)
+        input = json.loads(task['input'])
+        theory = input['theory']
+        size = input['size']
+
+        TODO('get atlas if not cached locally')
+        TODO('trim atlas to seed')
+        TODO('put seed')
+
+        result = {
+            'nextActivity': {
+                'activityType': '{}_{}_{}'.format('Grow', theory, size),
+                'input': {
+                    'seedFile': seed_file,
+                    },
+                }
+            }
+        pomagma.workflow.util.finish_activity_task(task, result)
+
+
+@parsable.command
+def start_aggregator(theory):
+    '''
+    Start aggregator, typically on a high-memory node.
+    WARNING There may be exactly one aggregator per theory.
+    '''
+    name = '{}_{}'.format('Aggregate', theory)
+    pomagma.workflow.util.register_activity_type(name)
+    while True:
+        task = pomagma.workflow.util.poll_activity_task(name)
+        input = json.loads(task['input'])
+
+        TODO('get atlas if not cached locally')
+        TODO('get seed if not cached locally')
+        TODO('aggregate')
+        TODO('put atlas')
+
+        pomagma.workflow.util.finish_activity_task(task)
+
+
+@parsable.command
+def start_grower(theory, size):
+    '''
+    Start grow worker, typically on a high-memory high-cpu node.
+    '''
+    workflow_name = 'Grow'
+    name = '{}_{}_{}'.format('Grow', theory, size)
+    param = json.dumps({'theory': theory, 'size': size})
+    pomagma.workflow.util.register_activity_type(name)
+    while True:
+        pomagma.workflow.util.start_workflow_execution(workflow_name, param)
+        task = pomagma.workflow.util.poll_activity_task(name)
+        input = json.loads(task['input'])
+
+        TODO('get seed')
+        TODO('grow')
+        TODO('put chart')
+
+        result = {
+            'nextActivity': {
+                'activityType': '{}_{}'.format('Aggregate', theory),
+                'input': {
+                    'chartFile': chart_file,
+                    },
+                }
+            }
+        pomagma.workflow.util.finish_activity_task(task, result)
+
+
+if __name__ == '__main__':
+    parsable.dispatch()
---
+++ src/workflow/util.py
@@ -0,0 +1,120 @@
+import uuid
+import boto.swf
+import boto.swf.layer1
+import boto.swf.layer1_decisions
+import boto.swf.exceptions
+
+
+DOMAIN = 'pomagma'
+VERSION = '1.0'
+#boto.swf.connect_to_region(REGION)
+SWF = boto.swf.layer1.Layer1()
+
+
+def get_hostip():
+    # Actors might request data be sent directly if possible else via s3
+    import netifaces
+    addresses = netifaces.ifaddresses('eth0')
+    return addresses[netifaces.AF_INET][0]['addr']
+
+
+def fork(fun, *args, **kwargs):
+    import multiprocessing
+    return multiprocessing.Process(target=fun, args=args, kwargs=kwargs)
+
+
+def onsigint(handler):
+    import signal
+    signal.signal(signal.SIGINT, handler)
+
+
+def random_uuid():
+    return str(uuid.uuid4())
+
+
+#-----------------------------------------------------------------------------
+# Admin
+
+
+def register_domain():
+    print 'Registering domain'
+    try:
+        SWF.register_domain(
+                name=DOMAIN,
+                workflow_execution_retention_period_in_days=90)
+    except boto.swf.exceptions.SWFDomainAlreadyExistsError:
+        pass
+
+
+def register_activity_type(name):
+    activity_type = '{}ActivityType'.format(name)
+    print 'Registering activity type {}'.format(activity_type)
+    try:
+        SWF.register_activity_type(DOMAIN, activity_type, VERSION)
+    except boto.swf.exceptions.SWFTypeAlreadyExistsError:
+        pass
+
+
+def register_workflow_type(name):
+    workflow_type = '{}ActivityType'.format(name)
+    print 'Registering workflow type {}'.format(workflow_type)
+    try:
+        SWF.register_workflow_type(DOMAIN, workflow_type, VERSION)
+    except boto.swf.exceptions.SWFTypeAlreadyExistsError:
+        pass
+
+
+def start_workflow_execution(workflow, version):
+    SWF.start_workflow_execution(
+        domain=DOMAIN,
+        workflow_id=random_uuid(),
+        workflow_name=workflow,
+        version=VERSION)
+
+#-----------------------------------------------------------------------------
+# Decisions
+
+def poll_decision_task(name):
+    task_list = '{}TaskList'.format(name)
+    while True:
+        response = SWF.poll_for_decision_task(DOMAIN, task_list)
+        if response['taskToken']:
+            print 'Decision Task: {}'.format(response)
+            return response
+
+
+def decide_to_schedule(decision_task, activity_type, input=None):
+    task_token = decision_task['taskToken']
+    decisions = boto.swf.layer1_decisions.Layer1Decisions()
+    task_list = '{}TaskList'.format(activity_type)
+    activity_id = random_uuid()
+    decisions.schedule_activity_task(
+        activity_id=activity_id,
+        activity_type_name='{}ActivityType'.format(activity_type),
+        activity_type_version=VERSION,
+        task_list=task_list,
+        input=input,
+        )
+    SWF.respond_decision_task_completed(task_token, decisions)
+
+
+def decide_to_complete(decision_task):
+    task_token = decision_task['taskToken']
+    decisions = boto.swf.layer1_decisions.Layer1Decisions()
+    decisions.complete_workflow_execution()
+    SWF.respond_decision_task_completed(task_token, decisions)
+
+#-----------------------------------------------------------------------------
+# Activities
+
+def poll_activity_task(name):
+    task_list = '{}TaskList'.format(name)
+    while True:
+        response = SWF.poll_for_activity_task(DOMAIN, task_list)
+        if response['taskToken']:
+            print 'Activity Task: {}'.format(response)
+            return response
+
+
+def finish_activity_task(task, result=None):
+    SWF.respond_activity_task_completed(task['taskToken'], result=result)
