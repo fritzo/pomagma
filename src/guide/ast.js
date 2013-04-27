af1a89080e89409de901334e3ccb322da00e756e
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-04-26 19:55:26 -0700

    Fix guide to pass unit tests

--- src/guide/ast.js
+++ src/guide/ast.js
@@ -107,39 +107,39 @@ Parser.prototype.pop = function () {
 
 Parser.prototype.parse = function () {
   var head = this.pop();
-  return new parse_head[head](this);
+  return parse_head[head](this);
 };
 
 var parse_head = {
   HOLE: function (state) {
-    return Hole();
+    return new Hole();
   },
   VARY: function(state) {
     var name = state.pop();
-    return Vary(name);
+    return new Vary(name);
   },
   ABSTRACT: function (state) {
     var patt = state.parse();
     var body = state.parse();
-    return Abstract(patt, body);
+    return new Abstract(patt, body);
   },
   APPLY: function (state) {
     var fun = state.parse();
     var arg = state.parse();
-    return Apply(fun, arg);
+    return new Apply(fun, arg);
   },
   QUOTE: function(state) {
     var body = state.parse();
-    return Quote(body);
+    return new Quote(body);
   },
   CURSOR: function(state) {
     var body = state.parse();
-    return Cursor(body);
+    return new Cursor(body);
   }
 };
 
 var parse = function (string) {
-  parser = new Parser(string);
+  var parser = new Parser(string);
   return parser.parse();
 };
 
@@ -150,6 +150,7 @@ test('Simple parsing', function(){
     ];
   for (var i = 0; i < cases.length; ++i) {
     var string = cases[i];
+    log('Parsing ' + string);
     var expr = parse(string);
     var polish = expr.polish();
     assertEqual(string, polish);
@@ -163,17 +164,18 @@ Cursor.prototype.remove = function () {
   var above = this.above;
   this.below[0].above = above;
   if (above) {
-    var pos = above.args.indexOf(this);
+    var pos = above.below.indexOf(this);
     above.below[pos] = this.below[0];
     return pos;
   }
 };
 
 Cursor.prototype.insert_below = function (above, pos) {
-  var arg = above.below[pos];
-  expr.below[pos] = this;
-  this.below[0] = arg;
-  arg.above = this;
+  var below = above.below[pos];
+  above.below[pos] = this;
+  this.above = above;
+  below.above = this;
+  this.below[0] = below;
 };
 
 Cursor.prototype.insert_above = function (below) {
@@ -182,8 +184,8 @@ Cursor.prototype.insert_above = function (below) {
   below.above = this;
   this.above = above;
   if (above !== null) {
-    var pos = above.args.indexOf(below);
-    above.below.args[pos] = this;
+    var pos = above.below.indexOf(below);
+    above.below.below[pos] = this;
   }
 };
 
@@ -217,7 +219,7 @@ Cursor.prototype.try_move_up = function () {
 
 Cursor.prototype.try_move_sideways = function (direction) {
   var pivot = this.above;
-  if ((pivot === null) || (pivot.args.length === 1)) {
+  if ((pivot === null) || (pivot.below.length === 1)) {
     return false;
   } else {
     var pos = this.remove();
@@ -245,13 +247,14 @@ Cursor.prototype.try_move = function (direction) {
 };
 
 test('Cursor movement', function(){
-  var start = 'ABSTRACT QUOTE VARY this APPLY VARY is APPLY VARY a VARY test';
-  var expr = parse(start);
+  var string = 'ABSTRACT QUOTE VARY this APPLY VARY is APPLY VARY a VARY test';
+  log('Traversing ' + string)
+  var expr = parse(string);
   var cursor = new Cursor(expr);
-  var path =  'UDDDULRUL';
-  var trace = '011010011';
+  var path =  'UDDDLRULDRDLDUUUU';
+  var trace = '01100011111101110';
   for (var i = 0; i < path.length; ++i) {
-    assertEqual(cursor.try_move(path[i]), trace[i]);
+    assertEqual(cursor.try_move(path[i]), Boolean(parseInt(trace[i])));
   }
 });
 
