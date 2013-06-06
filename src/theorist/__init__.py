a61c26bb1d03a7dfab8b90f30426077be3f9e466
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-06-05 22:30:01 -0700

    Move conjecture c++ code to theorist

--- src/theorist/__init__.py
+++ src/theorist/__init__.py
@@ -4,7 +4,7 @@ import pomagma.util
 
 def conjecture(theory, world_in, conjectures_out, **opts):
     pomagma.util.log_call(
-        os.path.join(pomagma.util.BIN, 'cartographer', 'conjecture'),
+        os.path.join(pomagma.util.BIN, 'theorist', 'conjecture'),
         pomagma.util.abspath(world_in),
         os.path.join(pomagma.util.LANGUAGE, '{}.language'.format(theory)),
         pomagma.util.abspath(conjectures_out),
