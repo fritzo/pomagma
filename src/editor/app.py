ba9bd36f1ae3cf7afe625bf9ab223c51403cacdb
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-07-07 23:13:26 -0700

    Remove broken bottle-cork-auth code

--- src/editor/app.py
+++ src/editor/app.py
@@ -1,25 +1,11 @@
 import os
 import bottle
-import beaker.middleware
 import pomagma.util
 import pomagma.corpus
-import pomagma.editor.auth
 
 
 PORT = int(os.environ.get('POMAGMA_EDITOR_PORT', 34934))
 STATIC = os.path.join(pomagma.util.SRC, 'editor', 'static')
-
-app = bottle.app()
-
-if pomagma.editor.auth.active:
-    session_options = {
-        'session.type': 'cookie',
-        'session.validate_key': True,
-        'session.cookie_expires': True,
-        'session.timeout': 3600 * 24,  # 1 day
-        'session.encrypt_key': pomagma.util.random_uuid(),
-    }
-    app = beaker.middleware.SessionMiddleware(app, session_options)
 
 
 @bottle.route('/info')
@@ -30,68 +16,46 @@ def _info():
     }
 
 
-@bottle.route('/login')
-def login_form():
-    return '''
-        <form method="POST" action="/login">
-        Username <input name="username" type="text" /> <br />
-        Password <input name="password" type="password" /> <br />
-        <input type="submit" />
-        </form>
-        '''
-
-
-@bottle.route('/login', method='POST')
-def login():
-    username = bottle.request.POST.get('username', '').strip()
-    password = bottle.request.POST.get('password', '').strip()
-    pomagma.editor.auth.login(
-        username,
-        password,
-        success_redirect='/',
-        fail_redirect='/login')
-
-
-@bottle.route('/logout')
-def logout():
-    pomagma.editor.auth.current_user.logout(redirect='/login')
+def require_auth():
+    if bottle.request.remote_route != ['127.0.0.1']:
+        raise bottle.HTTPError(403)
 
 
 @bottle.route('/test')
 def _test():
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     return bottle.static_file('test.html', root=STATIC)
 
 
 @bottle.route('/')
 def get_index():
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     return bottle.static_file('index.html', root=STATIC)
 
 
 @bottle.route('/static/<filepath:path>')
 def get_static(filepath):
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     return bottle.static_file(filepath, root=STATIC)
 
 
 @bottle.route('/corpus', method='GET')
 def get_corpus():
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     modules = pomagma.corpus.list_modules()
     return {'modules': modules}
 
 
 @bottle.route('/corpus/<module_name>', method='GET')
 def get_module(module_name):
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     module = pomagma.corpus.load_module(module_name)
     return {'module': module}
 
 
 @bottle.route('/corpus/<module_name>', method='PUT')
 def put_module(module_name):
-    pomagma.editor.auth.require(fail_redirect='/login')
+    require_auth()
     module = bottle.request.json
     assert module is not None, 'failed to store module {}'.format(module_name)
     pomagma.corpus.store_module(module_name, module)
@@ -101,4 +65,4 @@ def serve(port=PORT):
     '''
     Start editor server.
     '''
-    bottle.run(app=app, host='localhost', port=port, debug=True, reloader=True)
+    bottle.run(host='localhost', port=port, debug=True, reloader=True)
