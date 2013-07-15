999cbee9af8dcde9f3ad3bbc2d02f270f6a8c093
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-07-15 01:30:46 -0700

    Factor log out of safety.js

---
+++ src/editor/static/log.js
@@ -0,0 +1,37 @@
+var log = (function(){
+  var log;
+
+  if (this.document) { // in main window
+
+    if (window.console && window.console.log) {
+      log = function (message) {
+        listeners.forEach(function(cb){ cb(message); });
+        console.log(message);
+      };
+    } else {
+      log = function (message) {
+        listeners.forEach(function(cb){ cb(message); });
+      }; // ignore
+    }
+
+  } else { // in a web worker
+
+    log = function (message) {
+      listeners.forEach(function(cb){ cb(message); });
+      self.postMessage({'type':'log', 'data':message});
+    };
+  }
+
+  listeners = [];
+
+  log.pushListener = function (cb) {
+    listeners.push(cb);
+  };
+
+  log.popListener = function (cb) {
+    listeners.pop(cb);
+  };
+
+  return log;
+})();
+
