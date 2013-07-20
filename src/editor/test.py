659860db80193f1322b1cb6ec333bf8ae87237d3
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-07-20 15:10:47 -0700

    Make js tests robust to latency in require()

--- src/editor/test.py
+++ src/editor/test.py
@@ -32,12 +32,18 @@ def tearDown():
 @count_failures
 def test_all():
     browser.visit('http://localhost:34934/#test')
-    browser.execute_script('window.test = require("test");')
-    while not browser.evaluate_script('test.hasRun()'):
+    browser.execute_script(
+        '''
+        window.pytest = undefined;
+        require(["test"], function (test) {
+            window.pytest = test;
+        });
+        ''')
+    while not browser.evaluate_script('pytest && pytest.hasRun()'):
         print 'waiting...'
         time.sleep(0.1)
-    fail_count = browser.evaluate_script('test.failCount()')
+    fail_count = browser.evaluate_script('pytest.failCount()')
     assert fail_count == 0, '{} tests failed'.format(fail_count)
-    test_count = browser.evaluate_script('test.testCount()')
+    test_count = browser.evaluate_script('pytest.testCount()')
     assert test_count == TEST_COUNT,\
         'ERROR expected {} tests, actual {}'.format(TEST_COUNT, test_count)
