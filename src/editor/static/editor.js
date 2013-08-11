a148aaf784bb216920678f308c297e2619bd0c1c
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2013-08-11 11:10:18 -0700

    Refactor editor.js for internal modularity

--- src/editor/static/editor.js
+++ src/editor/static/editor.js
@@ -1,11 +1,91 @@
 define(['log', 'test', 'compiler', 'ast', 'corpus', 'navigate'],
 function(log,   test,   compiler,   ast,   corpus,   navigate)
 {
-  var cursorPos = 0;
   var ids = [];
   var asts = {};  // id -> ast
   var $lines = {};  // id -> dom node
   var cursor = null;
+  var cursorPos = 0;
+  var lineChanged = false;
+
+  //--------------------------------------------------------------------------
+  // Corpus Management
+
+  var load = function () {
+    ids = [];
+    asts = {};
+    corpus.findAllLines().forEach(function(id){
+      ids.push(id);
+      var line = corpus.findLine(id);
+      var lambda = compiler.loadLine(line);
+      asts[id] = ast.load(lambda);
+    });
+    assert(ids.length > 0, 'corpus is empty');
+  };
+
+  var replace = function (newTerm, subsForDash) {
+    if (subsForDash !== undefined) {
+      newTerm = compiler.substitute('&mdash;', subsForDash, newTerm);
+    }
+    log('DEBUG ' + compiler.print(newTerm));
+    TODO('replace old term with new');
+  };
+
+  var insertAssert = function () {
+    TODO('insert assertion below line;');
+    TODO('moveCursorLine to assertion');
+    TODO('move to assertion HOLE');
+  };
+
+  var insertDefine = function () {
+    TODO('insert definition below line;');
+    TODO('moveCursorLine to assertion');
+    TODO('move to assertion HOLE');
+  };
+
+  var removeAssert = function () {
+    TODO('remove assertion on current line');
+    TODO('move to next line');
+  };
+
+  var removeDefine = function () {
+    TODO('remove definition on current line');
+    TODO('move to next line');
+  };
+
+  var commitLine = function () {
+    var below = cursor.below[0];
+    ast.cursor.remove(cursor);
+    var line = ast.getRoot(below);
+    var lambda = ast.dump(line);
+    lambda = compiler.simplify(lambda);
+    line = ast.load(lambda);
+    ast.cursor.insertAbove(cursor, line);
+    renderLine();
+    lineChanged = false;
+  };
+
+  //--------------------------------------------------------------------------
+  // Rendering
+
+  var renderLine = function (id) {
+    if (id === undefined) {
+      id = ids[cursorPos];
+    }
+    var root = ast.getRoot(asts[id]);
+    var lambda = ast.dump(root);
+    var html = compiler.render(lambda);
+    $lines[id].html(html);
+  };
+
+  var renderAll = function () {
+    $lines = {};
+    var div = $('#code').empty()[0];
+    corpus.findAllLines().forEach(function(id){
+      $lines[id] = $('<pre>').attr('id', 'line' + id).appendTo(div);
+      renderLine(id);
+    });
+  };
 
   var sortLines = function (lineSet) {
     /*
@@ -25,102 +105,35 @@ function(log,   test,   compiler,   ast,   corpus,   navigate)
     return lineArray;
   };
 
-  var renderLine = function (id) {
-    if (id === undefined) {
-      id = ids[cursorPos];
-    }
-    var root = ast.getRoot(asts[id]);
-    var lambda = ast.dump(root);
-    var html = compiler.render(lambda);
-    $lines[id].html(html);
-  };
+  //--------------------------------------------------------------------------
+  // Cursor Movement
 
-  var removeCursor = function () {
-    if (cursor !== null) {
-      ast.cursor.remove(cursor);
-      var id = ids[cursorPos];
-      renderLine(id);
-    } else {
-      cursor = ast.cursor.create();
-    }
-  };
-
-  var moveLine = function (delta) {
-    removeCursor();
-    cursorPos = (cursorPos + ids.length + delta) % ids.length;
+  var initCursor = function () {
+    cursor = ast.cursor.create();
+    cursorPos = 0;
+    lineChanged = false;
     var id = ids[cursorPos];
-    log('moving cursor to id ' + id);
     ast.cursor.insertAbove(cursor, asts[id]);
     renderLine(id);
   };
 
-  var move = function (direction) {
-    // log('move: ' + direction);
+  var moveCursorLine = function (delta) {
+    if (lineChanged) {
+      commitLine();
+    }
+    ast.cursor.remove(cursor);
+    renderLine();
+    cursorPos = (cursorPos + ids.length + delta) % ids.length;
+    var id = ids[cursorPos];
+    //log('moving cursor to id ' + id);
+    ast.cursor.insertAbove(cursor, asts[id]);
+    renderLine(id);
+  };
+
+  var moveCursor = function (direction) {
     if (ast.cursor.tryMove(cursor, direction)) {
       renderLine();
-    } else {
-      //TODO('visual bell');
     }
-  };
-
-  var load = function () {
-    ids = [];
-    asts = {};
-    corpus.findAllLines().forEach(function(id){
-      ids.push(id);
-      var line = corpus.findLine(id);
-      var lambda = compiler.loadLine(line);
-      asts[id] = ast.load(lambda);
-    });
-    assert(ids.length > 0, 'corpus is empty');
-
-    var div = $('#code')[0];
-    $lines = {};
-    corpus.findAllLines().forEach(function(id){
-      $lines[id] = $('<pre>').attr('id', 'line' + id).appendTo(div);
-      renderLine(id);
-    });
-
-    cursorPos = 0;
-    moveLine(0);
-  };
-
-  var remove = function () {
-    if (cursor.above == null) {
-      TODO('remove line from corpus');
-    } else {
-      TODO('replace old term with hole');
-    }
-  };
-
-  var replace = function (newTerm, subs) {
-    if (subs !== undefined) {
-      newTerm = compiler.substitute('&mdash;', subs, newTerm);
-    }
-    log('DEBUG ' + compiler.print(newTerm));
-    TODO('replace old term with new');
-  };
-
-  var insertAssert = function () {
-    TODO('insert assertion below line;');
-    TODO('moveLine to assertion');
-    TODO('move to assertion HOLE');
-  };
-
-  var insertDefine = function () {
-    TODO('insert definition below line;');
-    TODO('moveLine to assertion');
-    TODO('move to assertion HOLE');
-  };
-
-  var removeAssert = function () {
-    TODO('remove assertion on current line');
-    TODO('move to next line');
-  };
-
-  var removeDefine = function () {
-    TODO('remove definition on current line');
-    TODO('move to next line');
   };
 
   //--------------------------------------------------------------------------
@@ -159,12 +172,12 @@ function(log,   test,   compiler,   ast,   corpus,   navigate)
 
     var generic = [
       ['escape', toggleHelp, 'toggle help'],
-      ['up', action(moveLine, -1), 'move up'],
-      ['down', action(moveLine, 1), 'move down'],
-      ['left', action(move, 'L'), 'move left'],
-      ['right', action(move, 'R'), 'move right'],
-      ['shift+left', action(move, 'U'), 'select'],
-      ['shift+right', action(move, 'U'), 'select'],
+      ['up', action(moveCursorLine, -1), 'move up'],
+      ['down', action(moveCursorLine, 1), 'move down'],
+      ['left', action(moveCursor, 'L'), 'move left'],
+      ['right', action(moveCursor, 'R'), 'move right'],
+      ['shift+left', action(moveCursor, 'U'), 'select'],
+      ['shift+right', action(moveCursor, 'U'), 'select'],
       ['A', action(insertAssert), render(ASSERT(HOLE))],
       ['D', action(insertDefine), render(DEFINE(VAR('...'), HOLE))]
     ];
@@ -176,9 +189,9 @@ function(log,   test,   compiler,   ast,   corpus,   navigate)
       }
     };
 
-    var on = function (name, term, subs) {
+    var on = function (name, term, subsForDash) {
       var callback = function () {
-        replace(term, subs);
+        replace(term, subsForDash);
         takeBearings();
       };
       var description = render(term);
@@ -235,19 +248,13 @@ function(log,   test,   compiler,   ast,   corpus,   navigate)
   //--------------------------------------------------------------------------
   // Interface
 
-  var main = function () {
-    load();
-    takeBearings();
-    $(window).off('keydown').on('keydown', navigate.trigger);
-  };
-
   return {
-    main: main,
-    //moveLine: moveLine,
-    //move: move,
-    //remove: remove,
-    //replace: replace,
-    //insertAssert: insertAssert,
-    //insertDefine: insertDefine,
+    main: function () {
+      load();
+      renderAll();
+      initCursor();
+      takeBearings();
+      $(window).off('keydown').on('keydown', navigate.trigger);
+    },
   };
 });
