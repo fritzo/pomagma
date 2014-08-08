ae22cdadd69c8c1aa8accefb3ec65b8407afd50d
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-08-07 23:51:11 -0700

    Try to pip install all of requirements.txt

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y \
-  cmake make g++ \
-  libtbb-dev \
-  libsparsehash-dev \
-  libprotobuf-dev protobuf-compiler python-protobuf \
-  libhdf5-serial-dev \
-  libssl-dev \
-  python-pip virtualenvwrapper \
-  python-tables \
-  graphviz \
-  gdb \
-  p7zip-full \
-  libzmq-dev \
-  nodejs \
-  #
+#sudo add-apt-repository -y ppa:chris-lea/node.js
+#sudo apt-get update
+#sudo apt-get install -y \
+#  cmake make g++ \
+#  libtbb-dev \
+#  libsparsehash-dev \
+#  libprotobuf-dev protobuf-compiler python-protobuf \
+#  libhdf5-serial-dev \
+#  libssl-dev \
+#  python-pip virtualenvwrapper \
+#  python-tables \
+#  graphviz \
+#  gdb \
+#  p7zip-full \
+#  libzmq-dev \
+#  nodejs \
+#  #
 
 if env | grep -q ^VIRTUAL_ENV=
 then
