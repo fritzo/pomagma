b58397e6b1a72eb6a53d00eda14d860e0d96fcc7
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-05-23 15:50:44 -0700

    Move definitions of Corpus:: classes out of Corpus

--- src/analyst/corpus.hpp
+++ src/analyst/corpus.hpp
@@ -11,71 +11,11 @@ namespace pomagma
 
 class Corpus
 {
-    class Dag;
-    class Reducer;
-    class Parser;
-
 public:
 
-    struct Term
-    {
-        enum Arity {
-            OB,
-            HOLE,
-            NULLARY_FUNCTION,
-            INJECTIVE_FUNCTION,
-            BINARY_FUNCTION,
-            SYMMETRIC_FUNCTION,
-            UNARY_RELATION,
-            BINARY_RELATION,
-            VARIABLE  // must be last to match CachedApproximator::Term::Arity
-        };
-
-        struct Hash
-        {
-            std::hash<std::string> hash_string;
-            std::hash<const Term *> hash_pointer;
-
-            uint64_t operator() (const Term & x) const
-            {
-                FNV_hash::HashState state;
-                state.add(x.arity);
-                state.add(hash_string(x.name));
-                state.add(hash_pointer(x.arg0));
-                state.add(hash_pointer(x.arg1));
-                state.add(x.ob);
-                return state.get();
-            }
-        };
-
-        bool operator== (const Term & o) const
-        {
-            return arity == o.arity
-               and name == o.name
-               and arg0 == o.arg0
-               and arg1 == o.arg1
-               and ob == o.ob;
-        }
-        bool operator!= (const Term & o) const { return not operator==(o); }
-
-        Term () {}
-        explicit Term (Ob o)
-            : arity(OB), name(), arg0(nullptr), arg1(nullptr), ob(o)
-        {}
-        Term (
-                Arity a,
-                const std::string & n = "",
-                const Term * a0 = nullptr,
-                const Term * a1 = nullptr)
-            : arity(a), name(n), arg0(a0), arg1(a1), ob(0)
-        {}
-
-        Arity arity;
-        std::string name;
-        const Term * arg0;
-        const Term * arg1;
-        Ob ob;
-    };
+    class Term;
+    class Linker;
+    class Histogram;
 
     template<class T>
     struct LineOf
@@ -84,58 +24,6 @@ public:
         T body;
 
         bool has_name () const { return not maybe_name.empty(); }
-    };
-
-    class Linker
-    {
-    public:
-        const Term * link (const Term * term);
-        const Term * approximate (const Term * term, size_t depth);
-    private:
-        friend class Corpus;
-        Linker (Dag & dag, std::vector<std::string> & error_log);
-        void define (const std::string & name, const Term * term);
-        void finish ();
-        const Term * approximate (const Term * term);
-        static void accum_free (
-                const Term * term,
-                std::unordered_set<const Term *> & free);
-
-        Dag & m_dag;
-        std::vector<std::string> & m_error_log;
-        std::unordered_map<const Term *, const Term *> m_definitions;
-        std::unordered_set<const Term *> m_ground_terms;
-
-        size_t m_temp_max_depth;
-        std::unordered_map<const Term *, size_t> m_temp_depths;
-    };
-
-    struct Histogram
-    {
-        std::unordered_map<std::string, size_t> symbols;
-        std::unordered_map<Ob, size_t> obs;
-
-        void add (const Term * term)
-        {
-            switch (term->arity) {
-                case Term::OB:
-                    ++obs[term->ob];
-                    break;
-
-                case Term::NULLARY_FUNCTION:
-                case Term::INJECTIVE_FUNCTION:
-                case Term::BINARY_FUNCTION:
-                case Term::SYMMETRIC_FUNCTION:
-                case Term::UNARY_RELATION:
-                case Term::BINARY_RELATION:
-                    ++symbols[term->name];
-                    break;
-
-                case Term::HOLE:
-                case Term::VARIABLE:
-                    break;
-            }
-        }
     };
 
     Corpus (Signature & signature);
@@ -154,8 +42,127 @@ public:
 
 private:
 
+    class Dag;
+    class Reducer;
+    class Parser;
+
     Signature & m_signature;
     Dag & m_dag;
+};
+
+struct Corpus::Term
+{
+    enum Arity {
+        OB,
+        HOLE,
+        NULLARY_FUNCTION,
+        INJECTIVE_FUNCTION,
+        BINARY_FUNCTION,
+        SYMMETRIC_FUNCTION,
+        UNARY_RELATION,
+        BINARY_RELATION,
+        VARIABLE  // must be last to match CachedApproximator::Term::Arity
+    };
+
+    struct Hash
+    {
+        std::hash<std::string> hash_string;
+        std::hash<const Term *> hash_pointer;
+
+        uint64_t operator() (const Term & x) const
+        {
+            FNV_hash::HashState state;
+            state.add(x.arity);
+            state.add(hash_string(x.name));
+            state.add(hash_pointer(x.arg0));
+            state.add(hash_pointer(x.arg1));
+            state.add(x.ob);
+            return state.get();
+        }
+    };
+
+    bool operator== (const Term & o) const
+    {
+        return arity == o.arity
+           and name == o.name
+           and arg0 == o.arg0
+           and arg1 == o.arg1
+           and ob == o.ob;
+    }
+    bool operator!= (const Term & o) const { return not operator==(o); }
+
+    Term () = default;
+    explicit Term (Ob o)
+        : arity(OB), name(), arg0(nullptr), arg1(nullptr), ob(o)
+    {}
+    explicit Term (
+            Arity a,
+            const std::string & n = "",
+            const Term * a0 = nullptr,
+            const Term * a1 = nullptr)
+        : arity(a), name(n), arg0(a0), arg1(a1), ob(0)
+    {}
+
+    Arity arity;
+    std::string name;
+    const Term * arg0;
+    const Term * arg1;
+    Ob ob;
+};
+
+class Corpus::Linker
+{
+public:
+
+    const Term * link (const Term * term);
+    const Term * approximate (const Term * term, size_t depth);
+
+private:
+
+    friend class Corpus;
+    Linker (Dag & dag, std::vector<std::string> & error_log);
+    void define (const std::string & name, const Term * term);
+    void finish ();
+    const Term * approximate (const Term * term);
+    static void accum_free (
+            const Term * term,
+            std::unordered_set<const Term *> & free);
+
+    Dag & m_dag;
+    std::vector<std::string> & m_error_log;
+    std::unordered_map<const Term *, const Term *> m_definitions;
+    std::unordered_set<const Term *> m_ground_terms;
+
+    size_t m_temp_max_depth;
+    std::unordered_map<const Term *, size_t> m_temp_depths;
+};
+
+struct Corpus::Histogram
+{
+    std::unordered_map<std::string, size_t> symbols;
+    std::unordered_map<Ob, size_t> obs;
+
+    void add (const Term * term)
+    {
+        switch (term->arity) {
+            case Term::OB:
+                ++obs[term->ob];
+                break;
+
+            case Term::NULLARY_FUNCTION:
+            case Term::INJECTIVE_FUNCTION:
+            case Term::BINARY_FUNCTION:
+            case Term::SYMMETRIC_FUNCTION:
+            case Term::UNARY_RELATION:
+            case Term::BINARY_RELATION:
+                ++symbols[term->name];
+                break;
+
+            case Term::HOLE:
+            case Term::VARIABLE:
+                break;
+        }
+    }
 };
 
 } // namespace pomagma
