4ded7ba3f4a54c670ed7b2ff5600547f229debf1
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-07-02 11:40:25 -0700

    Get rid of Broker prototype

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ sudo apt-get install -y \
   cmake make g++ ccache gdb \
   ctags cscope \
   libtbb-dev \
-  libstxxl-dev \
-  libsparsehash-dev \
-  libleveldb-dev \
-  libsnappy-dev \
   libprotobuf-dev \
   protobuf-compiler \
   python-protobuf \
@@ -23,6 +19,10 @@ sudo apt-get install -y \
   graphviz \
   p7zip-full \
   nodejs \
+  #libstxxl-dev \
+  #libsparsehash-dev \
+  #libleveldb-dev \
+  #libsnappy-dev \
   #
 
 if env | grep -q ^VIRTUAL_ENV=
