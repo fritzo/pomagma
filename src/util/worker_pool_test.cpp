c07233044fee829827f9697254cea17ba8de6499
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-09-21 22:30:48 -0700

    Sketch stronger WorkerPool test, currently failing

--- src/util/worker_pool_test.cpp
+++ src/util/worker_pool_test.cpp
@@ -1,5 +1,6 @@
 #include <pomagma/util/util.hpp>
 #include <pomagma/util/worker_pool.hpp>
+#include <atomic>
 
 using namespace pomagma;
 
@@ -10,14 +11,23 @@ void test_threadpool (
     POMAGMA_INFO("Testing pool of " << thread_count << " threads "
         << "with tasks taking up to " << max_duration << "ms");
     POMAGMA_ASSERT_LT(0, thread_count);
-    WorkerPool pool(thread_count);
-    for (size_t duration = 0; duration <= max_duration; ++duration) {
-        POMAGMA_DEBUG("scheduling sleep for " << duration << "ms");
-        pool.schedule([duration]{
-            POMAGMA_DEBUG("sleeping for " << duration << "ms");
-            std::this_thread::sleep_for(std::chrono::milliseconds(duration));
-        });
+
+    std::atomic<uint_fast64_t> counter(0);
+    {
+        WorkerPool pool(thread_count);
+        for (size_t duration = 0; duration <= max_duration; ++duration) {
+            POMAGMA_DEBUG("scheduling sleep for " << duration << "ms");
+            pool.schedule([duration, &counter]{
+                POMAGMA_DEBUG("sleeping for " << duration << "ms");
+                std::this_thread::sleep_for(
+                    std::chrono::milliseconds(duration));
+                size_t count = ++counter;
+                POMAGMA_DEBUG("count = " << count);
+            });
+        }
     }
+    // FIXME why does this assertion fail?
+    //POMAGMA_ASSERT_EQ(counter, 1 + max_duration);
 }
 
 int main ()
