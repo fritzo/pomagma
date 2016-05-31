ebf01a96c6614081cfc28847214ba4dda0996896
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-05-30 20:25:33 -0700

    Refactor reducer/lib_test.py to use @for_each decorator

--- src/reducer/lib_test.py
+++ src/reducer/lib_test.py
@@ -2,6 +2,7 @@ from pomagma.reducer import lib
 from pomagma.reducer.code import VAR
 from pomagma.reducer.engine import simplify
 from pomagma.reducer.sugar import as_code, app
+from pomagma.util.testing import for_each
 import pytest
 
 f = VAR('f')
@@ -14,7 +15,8 @@ undefined = lib.undefined
 true = lib.true
 false = lib.false
 
-INTRO_FORM_EXAMPLES = [
+
+@for_each([
     ('ok', lambda x: x),
     ('true', lambda x, y: x),
     ('false', lambda x, y: y),
@@ -27,126 +29,244 @@ INTRO_FORM_EXAMPLES = [
     ('succ', lambda n, z, s: app(s, n)),
     ('nil', lambda n, c: n),
     ('cons', lambda head, tail, n, c: app(c, head, tail)),
-]
-
-
-@pytest.mark.parametrize('name,native', INTRO_FORM_EXAMPLES)
+])
 def test_intro_forms(name, native):
     assert as_code(getattr(lib, name)) == as_code(native)
 
 
-def test_unit():
-    assert simplify(lib.unit_test(ok)) == ok
-    assert simplify(lib.unit_test(error)) == error
-    assert simplify(lib.unit_test(undefined)) == undefined
-    assert simplify(lib.unit_and(ok, ok)) == ok
-    assert simplify(lib.unit_and(ok, undefined)) == undefined
-    assert simplify(lib.unit_and(undefined, ok)) == undefined
-    assert simplify(lib.unit_and(undefined, undefined)) == undefined
-    assert simplify(lib.unit_or(ok, ok)) == ok
-    assert simplify(lib.unit_or(ok, undefined)) == ok
-    assert simplify(lib.unit_or(undefined, ok)) == ok
-    assert simplify(lib.unit_or(undefined, undefined)) == undefined
+# ----------------------------------------------------------------------------
+# Unit
+
+@for_each([
+    (ok, ok),
+    (error, error),
+    (undefined, undefined),
+])
+def test_unit_test(x, expected):
+    assert simplify(lib.unit_test(x)) == expected
 
 
-def test_bool():
-    assert simplify(lib.bool_test(true)) == ok
-    assert simplify(lib.bool_test(false)) == ok
-    assert simplify(lib.bool_test(error)) == error
-    assert simplify(lib.bool_test(undefined)) == undefined
-    assert simplify(lib.bool_not(true)) == false
-    assert simplify(lib.bool_not(false)) == true
-    assert simplify(lib.bool_and(true, true)) == true
-    assert simplify(lib.bool_and(true, false)) == false
-    assert simplify(lib.bool_and(false, true)) == false
-    assert simplify(lib.bool_and(false, false)) == false
-    assert simplify(lib.bool_or(true, true)) == true
-    assert simplify(lib.bool_or(true, false)) == true
-    assert simplify(lib.bool_or(false, true)) == true
-    assert simplify(lib.bool_or(false, false)) == false
+@for_each([
+    (ok, ok, ok),
+    (ok, undefined, undefined),
+    (undefined, ok, undefined),
+    (undefined, undefined, undefined),
+])
+def test_unit_and(x, y, expected):
+    assert simplify(lib.unit_and(x, y)) == expected
 
 
-def test_maybe():
-    assert simplify(lib.maybe_test(lib.none)) == ok
-    assert simplify(lib.maybe_test(lib.some(x))) == ok
-    assert simplify(lib.maybe_test(error)) == error
-    assert simplify(lib.maybe_test(undefined)) == undefined
+@for_each([
+    (ok, ok, ok),
+    (ok, undefined, ok),
+    (undefined, ok, ok),
+    (undefined, undefined, undefined),
+])
+def test_unit_or(x, y, expected):
+    assert simplify(lib.unit_or(x, y)) == expected
 
+
+# ----------------------------------------------------------------------------
+# Bool
+
+@for_each([
+    (true, ok),
+    (false, ok),
+    (error, error),
+    (undefined, undefined),
+])
+def test_bool_test(x, expected):
+    assert simplify(lib.bool_test(x)) == expected
+
+
+@for_each([
+    (true, false),
+    (false, true),
+])
+def test_bool_not(x, expected):
+    assert simplify(lib.bool_not(x)) == expected
+
+
+@for_each([
+    (true, true, true),
+    (true, false, false),
+    (false, true, false),
+    (false, false, false),
+])
+def test_bool_and(x, y, expected):
+    assert simplify(lib.bool_and(x, y)) == expected
+
+
+@for_each([
+    (true, true, true),
+    (true, false, true),
+    (false, true, true),
+    (false, false, false),
+])
+def test_bool_or(x, y, expected):
+    assert simplify(lib.bool_or(x, y)) == expected
+
+
+# ----------------------------------------------------------------------------
+# Maybe
+
+@for_each([
+    (lib.none, ok),
+    (lib.some(x), ok),
+    (error, error),
+    (undefined, undefined),
+])
+def test_maybe_test(x, expected):
+    assert simplify(lib.maybe_test(x)) == expected
+
+
+# ----------------------------------------------------------------------------
+# Products
 
 def test_prod():
     xy = lib.pair(x, y)
     assert simplify(lib.prod_test(xy)) == ok
     assert simplify(lib.prod_test(error)) == error
     assert simplify(lib.prod_test(undefined)) == undefined
+
     assert simplify(lib.prod_fst(xy)) == x
+
     assert simplify(lib.prod_snd(xy)) == y
 
 
-def test_sum():
-    assert simplify(lib.sum_test(lib.inl(x))) == ok
-    assert simplify(lib.sum_test(lib.inr(y))) == ok
-    assert simplify(lib.sum_test(error)) == error
-    assert simplify(lib.sum_test(undefined)) == undefined
+# ----------------------------------------------------------------------------
+# Sums
+
+@for_each([
+    (lib.inl(x), ok),
+    (lib.inr(y), ok),
+    (error, error),
+    (undefined, undefined),
+])
+def test_sum_test(x, expected):
+    assert simplify(lib.sum_test(x)) == expected
 
 
-def test_num():
-    succ = lib.succ
-    zero = lib.zero
-    one = succ(zero)
-    two = succ(one)
-    three = succ(two)
-    assert simplify(lib.num_test(zero)) == ok
-    assert simplify(lib.num_test(one)) == ok
-    assert simplify(lib.num_test(error)) == error
-    assert simplify(lib.num_test(succ(error))) == error
-    assert simplify(lib.num_test(succ(succ(error)))) == error
-    assert simplify(lib.num_test(undefined)) == undefined
-    assert simplify(lib.num_test(succ(undefined))) == undefined
-    assert simplify(lib.num_test(succ(succ(undefined)))) == undefined
-    assert simplify(lib.num_pred(zero)) == error
-    assert simplify(lib.num_pred(one)) == zero
-    assert simplify(lib.num_pred(two)) == one
-    assert simplify(lib.num_pred(three)) == two
-    assert simplify(lib.num_add(zero, zero)) == zero
-    assert simplify(lib.num_add(one, zero)) == one
-    assert simplify(lib.num_add(two, zero)) == two
-    assert simplify(lib.num_add(three, zero)) == three
-    assert simplify(lib.num_less(zero, zero)) == false
-    assert simplify(lib.num_less(zero, one)) == true
-    assert simplify(lib.num_less(zero, two)) == true
-    assert simplify(lib.num_less(one, zero)) == false
-    assert simplify(lib.num_less(one, one)) == false
-    assert simplify(lib.num_less(one, two)) == true
-    assert simplify(lib.num_less(two, zero)) == false
-    assert simplify(lib.num_less(two, one)) == false
+# ----------------------------------------------------------------------------
+# Numerals as Y Maybe
+
+succ = lib.succ
+zero = lib.zero
+one = succ(zero)
+two = succ(one)
+three = succ(two)
 
 
-@pytest.mark.xfail
-def test_num_xfail():
-    succ = lib.succ
-    zero = lib.zero
-    one = succ(zero)
-    two = succ(one)
-    three = succ(two)
-    assert simplify(lib.num_test(two)) == ok
-    assert simplify(lib.num_test(three)) == ok
-    assert simplify(lib.num_add(zero, two)) == two
-    assert simplify(lib.num_add(zero, three)) == three
-    assert simplify(lib.num_add(zero, one)) == one
-    assert simplify(lib.num_add(one, one)) == two
-    assert simplify(lib.num_add(one, two)) == three
-    assert simplify(lib.num_add(two, one)) == three
-    assert simplify(lib.num_less(two, two)) == false
+@for_each([
+    (zero, ok),
+    (one, ok),
+    pytest.mark.xfail((two, ok)),
+    pytest.mark.xfail((three, ok)),
+    (error, error),
+    (succ(error), error),
+    (succ(succ(error)), error),
+    (undefined, undefined),
+    (succ(undefined), undefined),
+    (succ(succ(undefined)), undefined),
+])
+def test_num_test(x, expected):
+    assert simplify(lib.num_test(x)) == expected
 
 
-def test_list():
-    nil = lib.nil
-    cons = lib.cons
-    assert simplify(lib.list_test(nil)) == ok
-    assert simplify(lib.list_test(cons(x, nil))) == ok
-    assert simplify(lib.list_test(error)) == error
-    assert simplify(lib.list_test(undefined)) == undefined
-    assert simplify(lib.list_test(cons(x, error))) == error
-    assert simplify(lib.list_test(cons(x, undefined))) == undefined
-    assert simplify(lib.list_empty(nil)) == true
-    assert simplify(lib.list_empty(cons(x, nil))) == false
+@for_each([
+    (zero, true),
+    (one, false),
+    (two, false),
+])
+def test_num_is_zero(x, expected):
+    assert simplify(lib.num_is_zero(x)) == expected
+
+
+@for_each([
+    (zero, error),
+    (one, zero),
+    (two, one),
+    (three, two),
+])
+def test_num_pred(x, expected):
+    assert simplify(lib.num_pred(x)) == expected
+
+
+@for_each([
+    (zero, zero, zero),
+    pytest.mark.xfail((zero, two, two)),
+    pytest.mark.xfail((zero, three, three)),
+    pytest.mark.xfail((zero, one, one)),
+    (one, zero, one),
+    pytest.mark.xfail((one, one, two)),
+    pytest.mark.xfail((one, two, three)),
+    (two, zero, two),
+    pytest.mark.xfail((two, one, three)),
+    (three, zero, three),
+])
+def test_num_add(x, y, expected):
+    assert simplify(lib.num_add(x, y)) == expected
+
+
+@for_each([
+    (zero, zero, true),
+    (zero, one, false),
+    (zero, two, false),
+    (one, zero, false),
+    (one, one, true),
+    (one, two, false),
+    (two, zero, false),
+    (two, one, false),
+    pytest.mark.xfail((two, two, true)),
+])
+def test_num_eq(x, y, expected):
+    assert simplify(lib.num_eq(x, y)) == expected
+
+
+@for_each([
+    (zero, zero, false),
+    (zero, one, true),
+    (zero, two, true),
+    (one, zero, false),
+    (one, one, false),
+    (one, two, true),
+    (two, zero, false),
+    (two, one, false),
+    pytest.mark.xfail((two, two, false)),
+])
+def test_num_less(x, y, expected):
+    assert simplify(lib.num_less(x, y)) == expected
+
+
+'''
+@for_each([
+])
+def test_(, expected):
+    assert simplify(lib.()) == expected
+'''
+
+# ----------------------------------------------------------------------------
+# Finite homogeneous lists
+
+nil = lib.nil
+cons = lib.cons
+
+
+@for_each([
+    (nil, ok),
+    (cons(x, nil), ok),
+    (error, error),
+    (undefined, undefined),
+    (cons(x, error), error),
+    (cons(x, undefined), undefined),
+])
+def test_list_test(x, expected):
+    assert simplify(lib.list_test(x)) == expected
+
+
+@for_each([
+    (nil, true),
+    (cons(x, nil), false),
+])
+def test_list_empty(x, expected):
+    assert simplify(lib.list_empty(x)) == expected
