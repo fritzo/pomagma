cb8b333e624856c6b6ab1c1e5efeb83832080391
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-05-30 20:05:53 -0700

    Reimplement @for_each decorator using pytest

--- src/util/testing.py
+++ src/util/testing.py
@@ -1,19 +1,17 @@
 import functools
+import inspect
+import pytest
 
 
 def for_each(examples):
 
     def decorator(fun):
+        args, vargs, kwargs, defaults = inspect.getargspec(fun)
+        if vargs or kwargs or defaults:
+            raise TypeError('Unsupported signature: {}'.format(fun))
+        argnames = ','.join(args)
+        return pytest.mark.parametrize(argnames, examples)(fun)
 
-        def fun_one(i):
-            fun(examples[i])
-
-        @functools.wraps(fun)
-        def decorated():
-            for i in xrange(len(examples)):
-                yield fun_one, i
-
-        return decorated
     return decorator
 
 
