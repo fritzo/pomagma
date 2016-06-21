e9a64236ce3683e517ccc238e7376d1c54e8b5ef
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-06-20 21:14:45 -0700

    Use varint for both fields of reducer.serial protocol

--- src/reducer/serial.py
+++ src/reducer/serial.py
@@ -1,16 +1,55 @@
-from pomagma.reducer.code import I, K, B, C, S, APP, is_app
+from pomagma.reducer.code import TOP, BOT, I, K, B, C, S, APP, JOIN
+from pomagma.reducer.code import is_app, is_join
+from pomagma.util import TODO
 
-ARGC_BITS = 2
-MAX_ARGC = (1 << ARGC_BITS) - 2
-ATOM_TO_INT = {
-    I: 0x1 << ARGC_BITS,
-    K: 0x2 << ARGC_BITS,
-    B: 0x3 << ARGC_BITS,
-    C: 0x4 << ARGC_BITS,
-    S: 0x5 << ARGC_BITS,
-}
-INT_TO_ATOM = {v: k for k, v in ATOM_TO_INT.iteritems()}
+PROTOCOL_VERSION = '0.0.1'  # Subject to backwards-incompatible change.
 
+
+# ----------------------------------------------------------------------------
+# Packed varints.
+
+ARGC_BITS = 3  # Up to 3 args and 16 atoms before varint overflow.
+HEAD_BITS = 8 - ARGC_BITS
+
+HEAD_MASK = 0xff >> ARGC_BITS
+ARGC_MASK = 0xff ^ HEAD_MASK
+
+# HEAD and ARGC are varint coded, after the initial bits.
+HEAD_OVERFLOW = 1 << (HEAD_BITS - 1)
+ARGC_OVERFLOW = 1 << (ARGC_BITS - 1)
+BYTE_OVERFLOW = 1 << (8 - 1)
+
+
+def _pack_bytes(head, argc):
+    if head >= HEAD_OVERFLOW:
+        TODO('support varint encoding of head')
+    if argc >= ARGC_OVERFLOW:
+        TODO('support varint encoding of argc')
+    yield chr(head | (argc << HEAD_BITS))
+
+
+def _unpack_bytes(source):
+    byte = ord(next(source))
+    head = byte & HEAD_MASK
+    argc = (byte & ARGC_MASK) >> HEAD_BITS
+    if head >= HEAD_OVERFLOW:
+        raise TODO('support varint decoding of head')
+    if argc >= ARGC_OVERFLOW:
+        raise TODO('support varint decoding of argc')
+    return head, argc
+
+
+# ----------------------------------------------------------------------------
+# Coding of atoms.
+
+J = intern('J')  # Simulates JOIN.
+
+INT_TO_ATOM = [TOP, BOT, I, K, B, C, S, J]
+ATOM_TO_INT = {k: v for v, k in enumerate(INT_TO_ATOM)}
+
+
+# ----------------------------------------------------------------------------
+# Dumping and loading.
 
 def dump(code, f):
     head = code
@@ -18,15 +57,17 @@ def dump(code, f):
     while is_app(head):
         args.append(head[2])
         head = head[1]
-    byte = len(args)
-    if byte > MAX_ARGC:
-        raise NotImplementedError(
-            'argc > {} is not implemented'.format(MAX_ARGC))
+    if is_join(head):
+        args.append(head[2])
+        args.append(head[1])
+        head = J
     try:
-        byte |= ATOM_TO_INT[head]
+        head = ATOM_TO_INT[head]
     except KeyError:
         raise ValueError('Failed to serialize code: {}'.format(code))
-    f.write(chr(byte))
+    argc = len(args)
+    for byte in _pack_bytes(head, argc):
+        f.write(byte)
     for arg in reversed(args):
         dump(arg, f)
 
@@ -41,13 +82,18 @@ def _iter_bytes(f, buffsize=8192):
 
 
 def _load_from(bytes_):
-    byte = ord(next(bytes_))
-    argc = byte & 0x3
-    head = byte & 0xFC
+    head, argc = _unpack_bytes(bytes_)
     try:
         head = INT_TO_ATOM[head]
-    except KeyError:
-        raise ValueError('Failed to deserialize byte: {:x}'.format(byte))
+    except IndexError:
+        raise ValueError('Unrecognized symbol: {}'.format(head))
+    if head is J:
+        if argc < 2:
+            TODO('support J with fewer than 2 args')
+        argc -= 2
+        x = _load_from(bytes_)
+        y = _load_from(bytes_)
+        head = JOIN(x, y)
     for _ in xrange(argc):
         arg = _load_from(bytes_)
         head = APP(head, arg)
--- src/reducer/serial_test.py
+++ src/reducer/serial_test.py
@@ -1,11 +1,13 @@
 from StringIO import StringIO
-from pomagma.reducer.code import I, K, B, C, S, APP
+from pomagma.reducer.code import TOP, BOT, I, K, B, C, S, APP, JOIN
 from pomagma.reducer.serial import dump
 from pomagma.reducer.serial import load
 from pomagma.util.testing import for_each
 
 
 @for_each([
+    TOP,
+    BOT,
     I,
     K,
     B,
@@ -13,11 +15,16 @@ from pomagma.util.testing import for_each
     S,
     APP(K, I),
     APP(APP(B, C), S),
+    APP(APP(APP(S, K), I), B),
+    JOIN(I, K),
+    JOIN(APP(K, I), K),
+    APP(JOIN(APP(K, I), K), I),
 ])
 def test_serialize_deserialize(code):
     f_out = StringIO()
     dump(code, f_out)
     serialized = f_out.getvalue()
+    print(' '.join('{:02x}'.format(ord(c)) for c in serialized))
     f_out.close()
 
     f_in = StringIO(serialized)
