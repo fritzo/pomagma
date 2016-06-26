caae8eef0c7de0bbdb71b017831778d753eb7333
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-06-25 18:21:05 -0700

    Add compile,decompile commands to pomagma.reducer

--- src/reducer/__main__.py
+++ src/reducer/__main__.py
@@ -1,4 +1,7 @@
 from parsable import parsable
+from pomagma.reducer import transforms
+from pomagma.reducer.code import parse
+from pomagma.reducer.code import serialize
 import os
 import pomagma.util
 import subprocess
@@ -16,6 +19,28 @@ def reduce(*args):
         trace = pomagma.util.get_stack_trace(binary, proc.pid)
         sys.stdout.write(trace)
     return proc.returncode  # Returns number of errors.
+
+
+@parsable
+def compile(string):
+    '''
+    Compile code from I,K,B,C,S to FUN,LET form.
+    '''
+    code = parse(string)
+    result = transforms.compile_(code)
+    print('In: {}'.format(string))
+    print('Out: {}'.format(serialize(result)))
+
+
+@parsable
+def decompile(string):
+    '''
+    Deompile code from FUN,LET to I,K,B,C,S form.
+    '''
+    code = parse(string)
+    result = transforms.deresult(code)
+    print('In: {}'.format(string))
+    print('Out: {}'.format(serialize(result)))
 
 
 if __name__ == '__main__':
