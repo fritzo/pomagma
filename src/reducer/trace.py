172c86181233fa0c230f8654fd928024d45e7549
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-07-08 23:54:49 -0700

    Add failing sketch of reducer.trace.trace_nondeterministic()

--- src/reducer/trace.py
+++ src/reducer/trace.py
@@ -1,4 +1,7 @@
-__all__ = ['trace_reduce']
+__all__ = [
+    'trace_deterministic',
+    'trace_nondeterministic',
+]
 
 from parsable import parsable
 from pomagma.reducer.code import TOP, BOT, I, K, B, C, S, J, VAR, APP, QUOTE
@@ -57,7 +60,7 @@ def drop_args(stack):
     return stack
 
 
-def trace_reduce(code):
+def trace_deterministic(code):
     init_fresh(code)
     state = REDUCE_HEAD
     stack = None
@@ -98,8 +101,6 @@ def trace_reduce(code):
                 y, stack = pop_arg(stack)
                 z, stack = pop_arg(stack)
                 code = APP(APP(x, z), APP(y, z))
-            elif code is J:
-                TODO()
             else:
                 raise ValueError(code)
         elif state is REDUCE_ARGS:
@@ -119,17 +120,157 @@ def trace_reduce(code):
                 code = QUOTE(code)
             else:
                 raise ValueError(op)
-    trace.append((state, code, stack))
+    return trace
+
+
+# ----------------------------------------------------------------------------
+# Scheduler as immutable stack = lifo queue (hence not complete)
+
+def schedule_init():
+    schedule = None
+    return schedule
+
+
+def schedule_is_empty(schedule):
+    return schedule is None
+
+
+def schedule_pop(schedule):
+    task, schedule = schedule
+    return schedule, task
+
+
+def schedule_peek(schedule):
+    task, schedule = schedule
+    return task
+
+
+def schedule_iter(schedule):
+    while not schedule_is_empty(schedule):
+        task, schedule = schedule_pop(schedule)
+        yield task
+
+
+def schedule_filter(schedule, pred):
+    if schedule is None:
+        return schedule
+        task, tail = schedule
+        filtered_tail = schedule_filter(tail, pred)
+        if filtered_tail == tail:
+            return schedule if pred(task) else tail
+        else:
+            tail = filtered_tail
+            return schedule_push(tail, task) if pred(task) else tail
+
+
+def try_decide_less(task_x, task_y):
+    """Weak oracle deciding ordering between tasks-as-continuations"""
+    if task_x[1] is BOT or task_y[1] is TOP or task_x == task_y:
+        return True
+    return None
+
+
+def schedule_push(schedule, task):
+    schedule = schedule_filter(
+        schedule,
+        lambda t: not try_decide_less(t, task),
+    )
+    if any(try_decide_less(task, t) for t in schedule_iter(schedule)):
+        return schedule
+    schedule = schedule_push(schedule, task)
+    return schedule
+
+
+def trace_nondeterministic(code):
+    TODO('this confuses the stack and schedule, and fails to recurse joins')
+    init_fresh(code)
+    state = REDUCE_HEAD
+    stack = None
+    task = state, code, stack
+    schedule = schedule_init()
+    schedule = schedule_push(schedule, task)
+    trace = []
+    while not schedule_is_empty(schedule):
+        trace.append(schedule)
+        schedule, task = schedule_pop(schedule)
+        state, code, stack = task
+        if state is REDUCE_HEAD:
+            if is_app(code):
+                stack = APP_ARG, code[2], stack
+                code = code[1]
+            elif is_quote(code):
+                stack = QUOTE_ARG, None, stack
+                code = code[1]
+            elif is_var(code):
+                state = REDUCE_ARGS
+            elif code is TOP or code is BOT:
+                stack = drop_args(stack)
+                state = REDUCE_ARGS
+            elif code is I:
+                x, stack = pop_arg(stack)
+                code = x
+            elif code is K:
+                x, stack = pop_arg(stack)
+                y, stack = pop_arg(stack)
+                code = x
+            elif code is B:
+                x, stack = pop_arg(stack)
+                y, stack = pop_arg(stack)
+                z, stack = pop_arg(stack)
+                code = APP(x, APP(y, z))
+            elif code is C:
+                x, stack = pop_arg(stack)
+                y, stack = pop_arg(stack)
+                z, stack = pop_arg(stack)
+                code = APP(APP(x, z), y)
+            elif code is S:
+                x, stack = pop_arg(stack)
+                y, stack = pop_arg(stack)
+                z, stack = pop_arg(stack)
+                code = APP(APP(x, z), APP(y, z))
+            elif code is J:
+                x, stack = pop_arg(stack)
+                y, stack = pop_arg(stack)
+                task = state, y, stack
+                schedule = schedule_push(schedule, task)
+                code = x
+            else:
+                raise ValueError(code)
+        elif state is REDUCE_ARGS:
+            if stack is None:
+                break
+            op, arg, stack = stack
+            if op is ABS_VAR:
+                code = abstract(arg, code)
+            elif op is APP_ARG:
+                fun = code
+                code = arg
+                stack = APP_FUN, fun, stack
+                state = REDUCE_HEAD
+            elif op is APP_FUN:
+                code = APP(arg, code)
+            elif op is QUOTE_ARG:
+                code = QUOTE(code)
+            else:
+                raise ValueError(op)
+        task = state, code, stack
+        schedule = schedule_push(schedule, task)
     return trace
 
 
 @parsable
-def trace_codes(sexpr):
+def trace_codes(sexpr, deterministic=True):
     """Print (state,code) pairs of a trace of reduction sequence."""
     code = sexpr_parse(sexpr)
-    trace = trace_reduce(code)
-    for state, code, stack in trace:
-        print '{} {}'.format(state, sexpr_print(code))
+    if deterministic:
+        for state, code, stack in trace_deterministic(code):
+            print '{} {}'.format(state, sexpr_print(code))
+    else:
+        for schedule in trace_nondeterministic(code):
+            for i, task in schedule_iter(schedule):
+                state, code, stack = task
+                prefix = '--' if i == 0 else '  '
+                print '{} {} {}'.format(prefix, state, sexpr_print(code))
 
 
 if __name__ == '__main__':
