cabf2f30b6507ccac691a335bc5444eff70cb7df
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-08-07 08:26:43 -0700

    Fork reducer.engine as reducer.continuation

---
+++ src/reducer/continuation.py
@@ -0,0 +1,409 @@
+__all__ = ['reduce', 'simplify']
+
+from collections import namedtuple
+from pomagma.compiler.util import memoize_arg
+from pomagma.compiler.util import memoize_args
+from pomagma.reducer import oracle
+from pomagma.reducer.code import CODE, EVAL, QQUOTE, QAPP, EQUAL, LESS
+from pomagma.reducer.code import TOP, BOT, I, K, B, C, S, J
+from pomagma.reducer.code import UNIT, BOOL, MAYBE
+from pomagma.reducer.code import VAR, APP, QUOTE
+from pomagma.reducer.code import free_vars, complexity
+from pomagma.reducer.code import is_var, is_atom, is_app, is_quote
+from pomagma.reducer.sugar import abstract
+from pomagma.reducer.util import LOG
+from pomagma.reducer.util import PROFILE_COUNTERS
+from pomagma.reducer.util import logged
+from pomagma.reducer.util import pretty
+import heapq
+import itertools
+
+F = APP(K, I)
+true = K
+false = F
+
+
+# ----------------------------------------------------------------------------
+# Immutable shared contexts
+
+@memoize_arg
+def make_var(n):
+    return VAR('v{}'.format(n))
+
+
+def fresh(avoid):
+    """Return the smallest variable not in avoid."""
+    PROFILE_COUNTERS[fresh, len(avoid)] += 1
+    for i in itertools.count():
+        var = make_var(i)
+        if var not in avoid:
+            return var
+
+
+Context = namedtuple('Context', ['stack', 'bound'])
+EMPTY_CONTEXT = Context(None, None)
+
+
+def context_pop(context, *terms):
+    if context.stack is not None:
+        arg, stack = context.stack
+        return arg, Context(stack, context.bound)
+    else:
+        avoid = frozenset()
+        for term in terms:
+            avoid |= free_vars(term)
+        arg = fresh(avoid)
+        bound = arg, context.bound
+        return arg, Context(context.stack, bound)
+
+
+def context_push(context, arg):
+    stack = arg, context.stack
+    return Context(stack, context.bound)
+
+
+def iter_shared_list(shared_list):
+    while shared_list is not None:
+        arg, shared_list = shared_list
+        yield arg
+
+
+def context_complexity(context):
+    result = 0
+    for arg in iter_shared_list(context.stack):
+        result += 1 + complexity(arg)  # APP(-, arg)
+    for var in iter_shared_list(context.bound):
+        result += 1 + complexity(var)  # FUN(var, -)
+    return result
+
+
+def continuation_complexity(continuation):
+    head, context = continuation
+    return complexity(head) + context_complexity(context)
+
+
+# ----------------------------------------------------------------------------
+# Priority queue for concurrent continuations
+
+class ContinuationQueue(object):
+    """Complexity-prioritized uniqueness-filtered continuation scheduler.
+
+    The lowest-complexity continuation is executed first.
+    Duplicates may be scheduled, but will only be executed once.
+    Uniqueness filtering has the nice effect of detecting some loops.
+    """
+    __slots__ = ['_to_pop', '_pushed']
+
+    def __init__(self):
+        self._to_pop = []
+        self._pushed = set()
+
+    def schedule(self, continuation):
+        if continuation not in self._pushed:
+            self._pushed.add(continuation)
+            priority = continuation_complexity(continuation)
+            heapq.heappush(self._to_pop, (priority, continuation))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            priority_task = heapq.heappop(self._to_pop)
+        except IndexError:
+            raise StopIteration
+        return priority_task[1]
+
+    next = __next__  # next() for python 2, __next__ for python 3.
+
+
+# ----------------------------------------------------------------------------
+# Reduction
+
+def try_unquote(code):
+    return code[1] if is_quote(code) else None
+
+
+TROOL_TO_CODE = {
+    True: true,
+    False: false,
+    None: None,
+}
+
+TRY_DECIDE = {
+    EQUAL: oracle.try_decide_equal,
+    LESS: oracle.try_decide_less,
+}
+
+TRY_CAST = {
+    BOOL: oracle.try_cast_bool,
+    UNIT: oracle.try_cast_unit,
+    MAYBE: oracle.try_cast_maybe,
+    CODE: oracle.try_cast_code,
+}
+
+
+def _sample(head, context, nonlinear):
+    """FIFO-scheduled sampler."""
+    # Head reduce.
+    while True:
+        # LOG.debug('head = {}'.format(pretty(head)))
+        PROFILE_COUNTERS[
+            _sample, head[0] if isinstance(head, tuple) else head] += 1
+        if is_app(head):
+            context = context_push(context, head[2])
+            head = head[1]
+        elif is_var(head):
+            yield head, context
+            return
+        elif is_quote(head):
+            x = head[1]
+            x = _reduce(x, False)
+            head = QUOTE(x)
+            yield head, context
+            return
+        elif head is TOP:
+            yield TOP, EMPTY_CONTEXT
+            return
+        elif head is BOT:
+            return
+        elif head is I:
+            head, context = context_pop(context)
+        elif head is K:
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            head = x
+        elif head is B:
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            z, context = context_pop(context, x, y)
+            head = x
+            context = context_push(context, _app(y, z, False))
+        elif head is C:
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            z, context = context_pop(context, x, y)
+            head = x
+            context = context_push(context, y)
+            context = context_push(context, z)
+        elif head is S:
+            old_context = context
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            z, context = context_pop(context, x, y)
+            if nonlinear or is_var(z):
+                head = x
+                context = context_push(context, _app(y, z, False))
+                context = context_push(context, z)
+            else:
+                yield head, old_context
+                return
+        elif head is J:
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            for head in (x, y):
+                for continuation in _sample(head, context, nonlinear):
+                    yield continuation
+        elif head is EVAL:
+            x, context = context_pop(context)
+            x = _reduce(x, nonlinear)
+            if is_quote(x):
+                head = x[1]
+            elif x is TOP:
+                yield TOP, EMPTY_CONTEXT
+                return
+            elif x is BOT:
+                return
+            else:
+                head = APP(EVAL, x)
+                yield head, context
+                return
+        elif head is QQUOTE:
+            x, context = context_pop(context)
+            x = _reduce(x, nonlinear)
+            if x is TOP:
+                yield TOP, EMPTY_CONTEXT
+                return
+            elif x is BOT:
+                return
+            elif is_quote(x):
+                head = QUOTE(x)
+            else:
+                head = APP(QQUOTE, x)
+                yield head, context
+                return
+        elif head is QAPP:
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            x = _reduce(x, nonlinear)
+            y = _reduce(y, nonlinear)
+            if is_quote(x) and is_quote(y):
+                head = QUOTE(_app(x[1], y[1], False))
+            else:
+                head = APP(APP(QAPP, x), y)
+                yield head, context
+                return
+        elif head in TRY_DECIDE:
+            pred = head
+            x, context = context_pop(context)
+            y, context = context_pop(context, x)
+            x = _reduce(x, nonlinear)
+            y = _reduce(y, nonlinear)
+            if x is TOP or y is TOP:
+                yield TOP, EMPTY_CONTEXT
+                return
+            if x is BOT and y is BOT:
+                return
+            if pred is EQUAL:
+                if (x is BOT and is_quote(y)) or (is_quote(x) and y is BOT):
+                    return
+            # FIXME This sometimes fails to err when it should; see (B3).
+            # FIXME This could be stronger by nonlinearly reducing the
+            #   arguments inside QUOTE(-) of the quoted terms; see (B4).
+            answer = TRY_DECIDE[pred](try_unquote(x), try_unquote(y))
+            head = TROOL_TO_CODE[answer]
+            if head is None:
+                head = APP(APP(pred, x), y)
+                yield head, context
+                return
+        elif head in TRY_CAST:
+            type_ = head
+            x, context = context_pop(context)
+            x = _reduce(x, nonlinear)
+            while is_app(x) and x[1] is type_:
+                x = x[2]
+            head = TRY_CAST[type_](x)
+            if head is None:
+                head = APP(type_, x)
+                yield head, context
+                return
+        else:
+            raise ValueError(head)
+
+
+def _schedule(head, context, queue):
+    for continuation in _sample(head, context, False):
+        queue.schedule(continuation)
+
+
+def _sample_nonlinear(head, context):
+    """Fair priority-scheduled nonlinear sampler."""
+    queue = ContinuationQueue()
+    _schedule(head, context, queue)
+    for head, context in queue:
+        if head is S:
+            x, context = context_pop(context)
+            y, context = context_pop(context)
+            z, context = context_pop(context)
+            assert not is_var(z), 'missed optimization'
+            head = x
+            context = context_push(context, _app(y, z, False))
+            context = context_push(context, z)
+            _schedule(head, context, queue)
+        else:
+            yield head, context
+
+
+def _close(continuation, nonlinear):
+    """Close a continuation in linear-beta-eta normal form."""
+    head, context = continuation
+    PROFILE_COUNTERS[_close, head[0] if isinstance(head, tuple) else head] += 1
+
+    # Reduce args.
+    for arg in iter_shared_list(context.stack):
+        # LOG.debug('head = {}'.format(pretty(head)))
+        arg = _reduce(arg, nonlinear)
+        head = APP(head, arg)
+
+    # Abstract free variables.
+    for var in iter_shared_list(context.bound):
+        # LOG.debug('head = {}'.format(pretty(head)))
+        head = abstract(var, head)
+
+    return head
+
+
+def _join(continuations, nonlinear):
+    PROFILE_COUNTERS[_join, '...'] += 1
+
+    # Collect unique samples.
+    samples = set()
+    for continuation in continuations:
+        sample = _close(continuation, nonlinear)
+        if sample is TOP:
+            return TOP
+        samples.add(sample)
+    if not samples:
+        return BOT
+
+    # Apply J-eta rules to recognize J and APP(J, x).
+    # This should really be combined with _close.
+    if K in samples and F in samples:
+        samples.remove(K)
+        samples.remove(F)
+        samples.add(J)
+    if I in samples:
+        for kx in tuple(samples):
+            if is_app(kx) and kx[1] is K:
+                samples.discard(I)
+                samples.remove(kx)
+                samples.add(APP(J, kx[2]))
+
+    # Filter out dominated samples.
+    # FIXME If x [= y and y [= x, this filters out both.
+    filtered_samples = []
+    for sample in samples:
+        if not any(oracle.try_decide_less(sample, other)
+                   for other in samples
+                   if other is not sample):
+            filtered_samples.append(sample)
+    filtered_samples.sort(key=lambda code: (complexity(code), code))
+
+    # Construct a join term.
+    result = filtered_samples[0]
+    for sample in filtered_samples[1:]:
+        result = APP(APP(J, result), sample)
+    return result
+
+
+# FIXME The fair scheduler in _sample_nonlinear does not work yet; see (B2).
+USE_FAIR_SCHEDULER = False
+
+
+@logged(pretty, pretty, returns=pretty)
+@memoize_args
+def _app(lhs, rhs, nonlinear):
+    head = lhs
+    context = EMPTY_CONTEXT
+    context = context_push(context, rhs)
+    if nonlinear and USE_FAIR_SCHEDULER:
+        samples = _sample_nonlinear(head, context)
+    else:
+        samples = _sample(head, context, nonlinear)
+    return _join(samples, nonlinear)
+
+
+@logged(pretty, returns=pretty)
+@memoize_args
+def _reduce(code, nonlinear):
+    if is_app(code):
+        return _app(code[1], code[2], nonlinear)
+    elif is_quote(code):
+        return QUOTE(_reduce(code[1], False))
+    elif is_atom(code) or is_var(code):
+        return code
+    else:
+        raise ValueError(code)
+
+
+def reduce(code, budget=0):
+    '''Beta-eta reduce code, ignoring budget.'''
+    assert isinstance(budget, int) and budget >= 0, budget
+    LOG.info('reduce({})'.format(pretty(code)))
+    return _reduce(code, True)
+
+
+def simplify(code):
+    '''Linearly beta-eta reduce.'''
+    LOG.info('simplify({})'.format(pretty(code)))
+    return _reduce(code, False)
---
+++ src/reducer/continuation_test.py
@@ -0,0 +1,285 @@
+from pomagma.reducer import continuation
+from pomagma.reducer import lib
+from pomagma.reducer.code import CODE, EVAL, QQUOTE, QAPP, EQUAL, LESS
+from pomagma.reducer.code import TOP, BOT, I, K, B, C, S, J
+from pomagma.reducer.code import UNIT, BOOL, MAYBE
+from pomagma.reducer.code import VAR, APP, QUOTE
+from pomagma.reducer.sugar import app, join, quote, qapp
+from pomagma.reducer.testing import iter_equations
+from pomagma.util.testing import for_each
+import hypothesis
+import hypothesis.strategies as s
+import pytest
+
+BUDGET = 10000
+
+w = VAR('w')
+x = VAR('x')
+y = VAR('y')
+z = VAR('z')
+F = app(K, I)
+
+
+@for_each([
+    (None, None, 0),
+    (None, None, 0),
+    (None, (x, None), 3),
+    (None, (y, (x, None)), 6),
+    ((I, None), None, 2),
+    ((I, None), (x, None), 2 + 3),
+    ((I, None), (y, (x, None)), 2 + 6),
+    ((x, None), None, 3),
+    ((x, None), (x, None), 3 + 3),
+    ((x, None), (y, (x, None)), 3 + 6),
+    ((x, (I, None)), None, 5),
+    ((x, (I, None)), (x, None), 5 + 3),
+    ((x, (I, None)), (y, (x, None)), 5 + 6),
+])
+def test_context_complexity(stack, bound, expected):
+    context = continuation.Context(stack=stack, bound=bound)
+    assert continuation.context_complexity(context) == expected
+
+
+def box(item):
+    return app(C, I, item)
+
+
+def enum(items):
+    return join(*map(box, items))
+
+
+REDUCE_EXAMPLES = [
+    (x, x),
+    (app(x, y), app(x, y)),
+    (app(x, I), app(x, I)),
+    (TOP, TOP),
+    (app(TOP, x), TOP),
+    (app(TOP, x, y), TOP),
+    (BOT, BOT),
+    (app(BOT, x), BOT),
+    (app(BOT, x, y), BOT),
+    (I, I),
+    (app(I, x), x),
+    (app(I, K), K),
+    (K, K),
+    (app(K, x), app(K, x)),
+    (app(K, x, y), x),
+    (app(K, x, y, z), app(x, z)),
+    (B, B),
+    (app(B, x), app(B, x)),
+    (app(B, x, y), app(B, x, y)),
+    (app(B, x, y, z), app(x, app(y, z))),
+    (app(B, x, y, z, w), app(x, app(y, z), w)),
+    (C, C),
+    (app(C, x), app(C, x)),
+    (app(C, x, y), app(C, x, y)),
+    (app(C, x, y, z), app(x, z, y)),
+    (app(C, x, y, z, w), app(x, z, y, w)),
+    (S, S),
+    (app(S, x), app(S, x)),
+    (app(S, x, y), app(S, x, y)),
+    (app(S, x, y, z), app(x, z, app(y, z))),
+    (app(S, x, y, z, w), app(x, z, app(y, z), w)),
+    (J, J),
+    (app(J, x), app(J, x)),
+    (join(x, y), join(x, y)),
+    (join(x, x), x),
+    (join(BOT, x), x),
+    (join(TOP, x), TOP),
+    (join(K, APP(K, I)), J),
+    (join(APP(K, I), K), J),
+    (join(J, K), J),
+    (join(K, J), J),
+    (join(J, APP(K, I)), J),
+    (join(APP(K, I), J), J),
+    (app(C, J), J),
+    (app(S, J, I), I),
+    (app(join(x, y), z), join(app(x, z), app(y, z))),
+    (app(join(I, app(K, I)), I), I),
+    (app(join(I, app(K, I)), K), join(I, K)),
+    (enum([I]), enum([I])),
+    (enum([I, I]), enum([I])),
+    (enum([I, BOT]), enum([I])),
+    (enum([I, TOP]), enum([TOP])),
+    (enum([BOT, I, TOP]), enum([TOP])),
+    (enum([K, F]), enum([K, F])),
+    (enum([BOT, K, F]), enum([K, F])),
+    (enum([K, F, J]), enum([J])),
+    (enum([K, F, TOP]), enum([TOP])),
+    (enum([BOT, box(BOT)]), enum([box(BOT)])),
+    (enum([BOT, box(BOT), box(box(BOT))]), enum([box(box(BOT))])),
+    (enum([BOT, box(BOT), box(box(BOT)), box(TOP)]), enum([box(TOP)])),
+    (app(lib.list_map, I, lib.nil), lib.nil),
+    (lib.list_map(I, lib.nil), lib.nil),
+    (lib.list_map(I, lib.cons(x, lib.nil)), lib.cons(x, lib.nil)),
+    (quote(x), quote(x)),
+    (quote(app(I, x)), quote(x)),
+    (app(EVAL, x), app(EVAL, x)),
+    (app(EVAL, TOP), TOP),
+    (app(EVAL, BOT), BOT),
+    (app(EVAL, quote(I), x), x),
+    (quote(app(EVAL, quote(app(K, I, I)))), quote(I)),
+    (app(S, K, K, quote(app(EVAL, quote(app(K, I, I))))), quote(I)),
+    (app(QQUOTE, x), app(QQUOTE, x)),
+    (app(QQUOTE, quote(x)), quote(quote(x))),
+    (app(QQUOTE, TOP), TOP),
+    (app(QQUOTE, BOT), BOT),
+    (qapp(x, y), qapp(x, y)),
+    (qapp(quote(x), y), qapp(quote(x), y)),
+    (qapp(x, quote(y)), qapp(x, quote(y))),
+    (qapp(quote(x), quote(y)), quote(app(x, y))),
+    (qapp(quote(S), quote(K), quote(K)), quote(I)),
+    (qapp(quote(S), app(I, quote(K)), app(EVAL, quote(quote(K)))), quote(I)),
+    (qapp(quote(lib.list_map), quote(I), quote(lib.nil)), quote(lib.nil)),
+    (app(EQUAL, x, y), app(EQUAL, x, y)),
+    (app(EQUAL, x, quote(y)), app(EQUAL, x, quote(y))),
+    (app(EQUAL, quote(x), y), app(EQUAL, quote(x), y)),
+    (app(EQUAL, quote(x), quote(y)), app(EQUAL, quote(x), quote(y))),
+    (app(EQUAL, quote(x), quote(x)), lib.true),
+    (app(EQUAL, quote(I), quote(K)), lib.false),
+    (app(EQUAL, TOP, x), TOP),
+    (app(EQUAL, x, TOP), TOP),
+    (app(EQUAL, BOT, x), app(EQUAL, BOT, x)),
+    (app(EQUAL, x, BOT), app(EQUAL, x, BOT)),
+    (app(EQUAL, BOT, quote(x)), BOT),
+    (app(EQUAL, quote(x), BOT), BOT),
+    (app(LESS, x, y), app(LESS, x, y)),
+    (app(LESS, x, quote(y)), app(LESS, x, quote(y))),
+    (app(LESS, quote(x), y), app(LESS, quote(x), y)),
+    (app(LESS, quote(x), quote(y)), app(LESS, quote(x), quote(y))),
+    (app(LESS, quote(x), quote(x)), lib.true),
+    (app(LESS, quote(BOT), quote(x)), lib.true),
+    (app(LESS, quote(x), quote(TOP)), lib.true),
+    (app(LESS, quote(TOP), quote(BOT)), lib.false),
+    (app(LESS, TOP, x), TOP),
+    (app(LESS, x, TOP), TOP),
+    (app(LESS, BOT, x), app(LESS, BOT, x)),
+    (app(LESS, x, BOT), app(LESS, x, BOT)),
+    (app(LESS, BOT, quote(x)), app(LESS, BOT, quote(x))),
+    (app(LESS, quote(x), BOT), app(LESS, quote(x), BOT)),
+    (app(LESS, BOT, quote(TOP)), lib.true),
+    (app(LESS, quote(BOT), BOT), lib.true),
+    pytest.mark.xfail(
+        (app(LESS, quote(TOP), quote(app(S, K, I, TOP))), lib.true),
+        reason='oracle is too weak',
+    ),
+    pytest.mark.xfail(
+        (app(LESS, quote(app(S, K, I, BOT)), quote(BOT)), lib.true),
+        reason='oracle is too weak',
+    ),
+    pytest.mark.xfail(
+        (app(LESS, quote(app(S, I, I, app(S, I, I))), quote(BOT)), lib.true),
+        reason='oracle is too weak',
+    ),
+    (UNIT, UNIT),
+    (app(UNIT, x), app(UNIT, x)),
+    (app(UNIT, app(UNIT, x)), app(UNIT, x)),
+    (app(UNIT, TOP), TOP),
+    (app(UNIT, BOT), BOT),
+    (app(UNIT, I), I),
+    (app(UNIT, K), TOP),
+    (app(UNIT, B), TOP),
+    (app(UNIT, C), TOP),
+    (app(UNIT, S), TOP),
+    (app(UNIT, J), TOP),
+    (app(BOOL, x), app(BOOL, x)),
+    (app(BOOL, app(BOOL, x)), app(BOOL, x)),
+    (app(BOOL, TOP), TOP),
+    (app(BOOL, BOT), BOT),
+    (app(BOOL, K), K),
+    (app(BOOL, APP(K, I)), APP(K, I)),
+    (app(BOOL, I), TOP),
+    (app(BOOL, B), TOP),
+    (app(BOOL, C), TOP),
+    (app(BOOL, S), TOP),
+    (app(BOOL, J), TOP),
+    (app(MAYBE, x), app(MAYBE, x)),
+    (app(MAYBE, app(MAYBE, x)), app(MAYBE, x)),
+    (app(MAYBE, TOP), TOP),
+    (app(MAYBE, BOT), BOT),
+    (app(MAYBE, K), K),
+    (app(MAYBE, app(K, app(C, I, TOP))), app(K, app(C, I, TOP))),
+    (app(MAYBE, app(K, app(C, I, BOT))), app(K, app(C, I, BOT))),
+    (app(MAYBE, app(K, app(C, I, I))), app(K, app(C, I, I))),
+    (app(MAYBE, I), TOP),
+    (app(MAYBE, B), TOP),
+    (app(MAYBE, C), TOP),
+    (app(MAYBE, S), TOP),
+    (app(MAYBE, J), TOP),
+    (app(CODE, x), app(CODE, x)),
+    (app(CODE, app(CODE, x)), app(CODE, x)),
+    (app(CODE, TOP), TOP),
+    (app(CODE, BOT), BOT),
+    (app(CODE, QUOTE(x)), QUOTE(x)),
+    (app(CODE, QUOTE(TOP)), QUOTE(TOP)),
+    (app(CODE, QUOTE(BOT)), QUOTE(BOT)),
+    (app(CODE, QUOTE(I)), QUOTE(I)),
+    (app(CODE, QUOTE(QUOTE(x))), QUOTE(QUOTE(x))),
+    (app(CODE, app(QQUOTE, x)), app(QQUOTE, x)),
+    (app(CODE, qapp(x, y)), qapp(x, y)),
+]
+
+
+@for_each(REDUCE_EXAMPLES)
+def test_reduce(code, expected_result):
+    actual_result = continuation.reduce(code, BUDGET)
+    assert actual_result == expected_result
+
+
+@for_each(iter_equations(['sk', 'join', 'quote'], test_id='continuation'))
+def test_trace_reduce_equations(lhs, rhs, message):
+    lhs = continuation.reduce(lhs)
+    rhs = continuation.reduce(rhs)
+    assert lhs == rhs, message
+
+
+alphabet = '_abcdefghijklmnopqrstuvwxyz'
+s_vars = s.builds(
+    VAR,
+    s.builds(str, s.text(alphabet=alphabet, min_size=1, average_size=5)),
+)
+s_atoms = s.one_of(
+    s.one_of(s_vars),
+    s.just(TOP),
+    s.just(BOT),
+    s.just(I),
+    s.just(K),
+    s.just(B),
+    s.just(C),
+    s.just(S),
+    s.just(J),
+    s.one_of(
+        s.just(CODE),
+        s.just(EVAL),
+        s.just(QAPP),
+        s.just(QQUOTE),
+        s.just(EQUAL),
+        s.just(LESS),
+    ),
+    s.one_of(
+        s.just(UNIT),
+        s.just(BOOL),
+        s.just(MAYBE),
+    ),
+)
+
+
+def s_codes_extend(codes):
+    return s.one_of(
+        s.builds(APP, codes, codes),
+        s.builds(QUOTE, codes),
+    )
+
+
+s_codes = s.recursive(s_atoms, s_codes_extend, max_leaves=100)
+s_quoted = s.builds(quote, s_codes)
+
+
+@hypothesis.given(s_codes)
+def test_simplify_runs(code):
+    continuation.simplify(code)
+
+
+@hypothesis.given(s_quoted)
+def test_simplify_runs_quoted(quoted):
+    continuation.simplify(quoted)
