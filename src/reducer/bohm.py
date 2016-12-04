88d8e3b8b2385f514aba11f9f9b681cdc6befa9a
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-12-04 14:38:56 -0800

    Sketch more of reducer.bohm (untested)

--- src/reducer/bohm.py
+++ src/reducer/bohm.py
@@ -10,13 +10,17 @@ CHANGELOG
 2016-12-04 Initial prototype.
 """
 
-from pomagma.compiler.util import memoize_args
+from pomagma.compiler.util import memoize_arg, memoize_args
 from pomagma.reducer.code import (
-    TOP, BOT, IVAR, APP, JOIN, ABS,
-    is_atom, is_nvar, is_ivar, is_app, is_abs, is_join, is_quote,
+    TOP, BOT, IVAR, APP, JOIN, ABS, QUOTE,
+    is_code, is_atom, is_nvar, is_ivar, is_app, is_abs, is_join, is_quote,
+    complexity,
 )
 from pomagma.util import TODO
 
+
+# ----------------------------------------------------------------------------
+# Functional programming
 
 @memoize_args
 def increment_rank(code, min_rank):
@@ -69,26 +73,36 @@ def decrement_rank(code, min_rank):
     return result
 
 
-def is_const(code):
-    return try_decrement_rank(code, 0) is None
+def is_const(code, rank=0):
+    return try_decrement_rank(code, rank) is None
 
 
-# TODO replace subs by a linear define(-,-,-), and simplify app(-,-).
+def is_cheap_to_copy(code):
+    return complexity(code) <= 6  # 6 = complexity(S)
+
+
 @memoize_args
-def subs(body, value, rank):
+def substitute(body, value, rank):
+    """Linearly substitute value for IVAR(rank) in body."""
     if is_ivar(body):
         return value if body[1] == rank else body
     elif is_atom(body) or is_nvar(body) or is_quote(body):
         return body
     elif is_app(body):
-        lhs = subs(body[1], value, rank)
-        rhs = subs(body[2], value, rank)
-        return app(lhs, rhs)
+        lhs = body[1]
+        rhs = body[2]
+        if (is_cheap_to_copy(value) or is_const(lhs, rank) or
+                is_const(rhs, rank)):
+            lhs = substitute(lhs, value, rank)
+            rhs = substitute(rhs, value, rank)
+            return app(lhs, rhs)
+        else:
+            return APP(ABS(body), value)
     elif is_abs(body):
-        return abstract(subs(body, increment_rank(value, 0), rank + 1))
+        return abstract(substitute(body, increment_rank(value, 0), rank + 1))
     elif is_join(body):
-        lhs = subs(body[1], value, rank)
-        rhs = subs(body[2], value, rank)
+        lhs = substitute(body[1], value, rank)
+        rhs = substitute(body[2], value, rank)
         return join(lhs, rhs)
     else:
         raise ValueError(body)
@@ -109,20 +123,8 @@ def app(fun, key):
         rhs = app(fun[2], key)
         return join(lhs, rhs)
     elif is_abs(fun):
-        # Linearly reduce.
         body = fun[1]
-        if is_ivar(body):
-            rank = body[1]
-            return key if rank == 0 else IVAR(rank - 1)
-        elif is_app(body):
-            lhs = body[1]
-            rhs = body[2]
-            if is_cheap_to_copy(key) or is_const(lhs) or is_const(rhs):
-                return subs(body, key, 0)
-            else:
-                return app(fun, key)
-        else:
-            raise ValueError(body)
+        return substitute(body, key)
     else:
         raise ValueError(fun)
 
@@ -143,15 +145,8 @@ def abstract(body):
         return ABS(body)
 
 
-@memoize_args
-def join(lhs, rhs):
-    codes = set()
-    for term in iter_join(lhs):
-        codes.add(term)
-    for term in iter_join(rhs):
-        codes.add(term)
-    return join_codes(codes)
-
+# ----------------------------------------------------------------------------
+# Scott ordering
 
 def iter_join(code):
     if is_join(code):
@@ -163,11 +158,209 @@ def iter_join(code):
         yield code
 
 
-def join_codes(codes):
-    if not isinstance(codes, set):
-        raise ValueError(codes)
-    TODO('adapt logic from engines.de_bruijn')
+@memoize_args
+def join(lhs, rhs):
+    """Join two codes, modulo linear Scott ordering."""
+
+    # Destructure all JOIN terms.
+    codes = set()
+    for term in iter_join(lhs):
+        codes.add(term)
+    for term in iter_join(rhs):
+        codes.add(term)
+    if not codes:
+        return BOT
+    if len(codes) == 1:
+        return next(iter(codes))
+
+    # Filter out strictly dominated codes.
+    filtered_codes = [
+        code for code in codes
+        if not any(dominates(ub, code) for ub in codes if ub is not code)
+    ]
+    filtered_codes.sort(key=priority, reverse=True)
+
+    # Construct a JOIN term.
+    result = filtered_codes[0]
+    for code in filtered_codes[1:]:
+        result = JOIN(code, result)
+    return result
 
 
-def is_cheap_to_copy(code):
+def dominates(lhs, rhs):
+    """Strict domination relation."""
+    lhs_rhs = try_decide_less(lhs, rhs)
+    rhs_lhs = try_decide_less(rhs, lhs)
+    return rhs_lhs is True and (lhs_rhs is False or lhs < rhs)
+
+
+@memoize_args
+def try_decide_less(lhs, rhs):
+    """Weak decision oracle for Scott ordering.
+
+            | TOP   IVAR   NVAR   APP-ABS
+    --------+---------------------------
+        TOP | True  False  None   approx
+       IVAR | True  delta  False  approx
+       NVAR | True  False  ...    approx
+    APP-ABS | True  approx approx approx
+
+    Theorem: (soundness)
+      - If try_decide_less(lhs, rhs) = True, then lhs [= rhs.
+      - If try_decide_less(lhs, rhs) = False, then lhs [!= rhs.
+    Theorem: (linear completeness)
+      - If lhs [= rhs and both are linear,
+        then try_decide_less(lhs, rhs) = True.
+      - If lhs [!= rhs and both are linear,
+        then try_decide_less(lhs, rhs) = False.
+    Desired Theorem: (strong linear completeness)
+      - If lhs [= u [= v [= rhs for some linear u, v,
+        then try_decide_less(lhs, rhs) = True.
+      - If rhs [= u [!= v [= lhs for some linear u, v,
+        then try_decide_less(lhs, rhs) = False.
+
+    Args:
+        lhs, rhs : code
+    Returns:
+        True, False, or None
+
+    """
+    assert is_code(lhs), lhs
+    assert is_code(rhs), rhs
+
+    # Try simple cases.
+    if lhs is BOT or lhs is rhs or rhs is TOP:
+        return True
+    if lhs is TOP and rhs is BOT:
+        return False
+
+    # Destructure lhs.
     TODO()
+
+    # Destructure rhs.
+    TODO()
+
+    # Try incompatible cases.
+    TODO()
+    # if lhs is TOP and is_ivar(rhs_head):
+    #     return False
+    # if is_ivar(lhs_head) and rhs is BOT:
+    #     return False
+
+    # Eta expand until binder counts agree.
+    # for _ in xrange(rhs_bound - lhs_bound):
+    #     lhs_head = code_increment_ivars(lhs_head, 0)
+    #     lhs_stack = stack_increment_ivars(lhs_stack, 0)
+    # for _ in xrange(lhs_bound - rhs_bound):
+    #     rhs_head = code_increment_ivars(rhs_head, 0)
+    #     rhs_stack = stack_increment_ivars(rhs_stack, 0)
+
+    # # Check for unknowns.
+    # if is_nvar(lhs_head) or is_nvar(rhs_head):
+    #     if lhs_head is rhs_head:
+    #         return stack_try_decide_less(lhs_stack, rhs_stack)
+    #     return None
+
+    # # Try comparing stacks.
+    # assert is_ivar(lhs_head) or lhs_head in INERT_ATOMS, lhs_head
+    # assert is_ivar(rhs_head) or rhs_head in INERT_ATOMS, rhs_head
+    # if is_ivar(lhs_head) and is_ivar(rhs_head):
+    #     if lhs_head is not rhs_head:
+    #         return False
+    #     return stack_try_decide_less(lhs_stack, rhs_stack)
+
+    # # Try approximating S.
+    # assert lhs_head is S or rhs_head is S
+    # if lhs_head is S and rhs_head is S:
+    #     # Try to compare assuming lhs and rhs align.
+    #     if stack_try_decide_less(lhs_stack, rhs_stack) is True:
+    #         return True
+    # if lhs_head is S:
+    #     # Try to approximate lhs.
+    #     for lhs_ub in iter_head_upper_bounds(lhs):
+    #         if cont_try_decide_less(lhs_ub, rhs) is True:
+    #             return True
+    #     for lhs_lb in iter_head_lower_bounds(lhs):
+    #         if cont_try_decide_less(lhs_lb, rhs) is False:
+    #             return False
+    # if rhs_head is S:
+    #     # Try to approximate rhs.
+    #     for rhs_lb in iter_head_lower_bounds(rhs):
+    #         if cont_try_decide_less(lhs, rhs_lb) is True:
+    #             return True
+    #     for rhs_ub in iter_head_upper_bounds(rhs):
+    #         if cont_try_decide_less(lhs, rhs_ub) is False:
+    #             return False
+
+    # Give up.
+    return None
+
+
+# ----------------------------------------------------------------------------
+# Computation
+
+def priority(code):
+    return is_normal(code), complexity(code), code
+
+
+@memoize_arg
+def is_normal(code):
+    if is_ivar(code) or is_nvar(code):
+        return True
+    elif is_abs(code):
+        return is_normal(code[1])
+    elif is_app(code):
+        if is_abs(code[1]):
+            return False
+        return is_normal(code[1]) and is_normal(code[2])
+    elif is_join(code):
+        return is_normal(code[1]) and is_normal(code[2])
+    elif is_quote(code):
+        return is_normal(code[1])
+    else:
+        raise ValueError(code)
+
+
+@memoize_arg
+def try_compute_step(code):
+    if not is_normal(code):
+        return None
+    if is_app(code):
+        fun = code[1]
+        arg = code[2]
+        if is_abs(fun):
+            body = fun[1]
+            assert is_app(body), code
+            lhs = body[1]
+            rhs = body[2]
+            return app(substitute(lhs, arg, 0), substitute(rhs, arg, 0))
+        else:
+            result = try_compute_step(fun)
+            if result is not None:
+                return app(result, arg)
+            result = try_compute_step(arg)
+            if result is not None:
+                return app(fun, result)
+            raise RuntimeError(code)
+    elif is_join(code):
+        lhs = code[1]
+        rhs = code[2]
+        result = try_compute_step(lhs)
+        if result is not None:
+            return join(result, rhs)
+        result = try_compute_step(rhs)
+        if result is not None:
+            return join(lhs, result)
+        raise RuntimeError(code)
+    elif is_abs(code):
+        result = try_compute_step(code[1])
+        if result is not None:
+            return ABS(result)
+        raise RuntimeError(code)
+    elif is_quote(code):
+        result = try_compute_step(code[1])
+        if result is not None:
+            return QUOTE(result)
+        raise RuntimeError(code)
+    else:
+        raise ValueError(code)
