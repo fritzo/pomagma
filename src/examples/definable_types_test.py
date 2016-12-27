bc9dc3a4a56f6b4a672499d4bc86786aacf21e2c
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-12-26 17:14:27 -0800

    Add examples.definable_types experiment

---
+++ src/examples/definable_types_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pomagma.examples.definable_types import trace
+from pomagma.util.testing import for_each
+
+
+@for_each([
+    (('base',), {}),
+    (('base', 'copy'), {}),
+    (('base', 'bot'), {}),
+    (('base', 'copy', 'bot'), {}),
+    (('base', 'div'), {}),
+    (('base', 'swap'), {}),
+    (('base', 'preconj'), {}),
+    (('base', 'postconj'), {}),
+    pytest.mark.xfail((('base', 'compose'), {})),
+    pytest.mark.xfail((('all',), {'steps': 1})),
+])
+def test_trace_runs(args, kwargs):
+    trace(*args, **kwargs)
