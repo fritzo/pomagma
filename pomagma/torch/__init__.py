6111dae9e6116dceb7f2a56a3c117ceceaa6c364
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2025-06-14 17:06:54 -0700

    Attempt to fix hang in CI

--- pomagma/torch/__init__.py
+++ pomagma/torch/__init__.py
@@ -1,8 +1,13 @@
 import os
+import warnings
 
 import torch
 
 from pomagma.util import BLOB_DIR, BUILD
 
-torch.ops.load_library(os.path.join(BUILD, "src", "torch", "pomagma_torch.so"))
-torch.ops.pomagma.init_extension(BLOB_DIR)
+POMAGMA_TORCH_SO = os.path.join(BUILD, "src", "torch", "pomagma_torch.so")
+if os.path.exists(POMAGMA_TORCH_SO):
+    torch.ops.load_library(POMAGMA_TORCH_SO)
+    torch.ops.pomagma.init_extension(BLOB_DIR)
+else:
+    warnings.warn(f"PyTorch extension not found: {POMAGMA_TORCH_SO}")
